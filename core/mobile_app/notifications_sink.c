7fd2d6c111b46bd6972bdb14be4b51861adab0a5
stefkosdev <ps@friendup.cloud>
2018-12-10 11:02:05 +0100

    Crash fix

--- core/mobile_app/notifications_sink.c
+++ core/mobile_app/notifications_sink.c
@@ -149,7 +149,7 @@ int WebsocketNotificationsSinkCallback( struct lws *wsi, enum lws_callback_reaso
 				if( e != NULL )
 				{
 					//DEBUG("Release: %p\n", e->fq_Data );
-					if( d->fq__Data != NULL )
+					if( e->fq__Data != NULL )
 					{
 						FFree( e->fq_Data );
 						e->fq_Data = NULL;
