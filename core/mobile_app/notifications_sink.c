7bff397d4a767b76a2fe06e58503064c25342664
stefkosdev <ps@friendup.cloud>
2018-12-11 11:00:05 +0100

    WS - presence messages fix

--- core/mobile_app/notifications_sink.c
+++ core/mobile_app/notifications_sink.c
@@ -64,6 +64,7 @@ static inline int WriteMessage( DataQWSIM *d, unsigned char *msg, int len )
 			en->fq_Data = FMalloc( len+32+LWS_SEND_BUFFER_PRE_PADDING+LWS_SEND_BUFFER_POST_PADDING );
 			memcpy( en->fq_Data+LWS_SEND_BUFFER_PRE_PADDING, msg, len );
 			en->fq_Size = len;
+			FERROR("\t\t\t\t\t\t\t\t\t\t\tSENDMESSSAGE\n<%s> size: %d\n\n\n\n", en->fq_Data+LWS_SEND_BUFFER_PRE_PADDING, len );
 	
 			//FQPushFIFO( &(man->man_Queue), en );
 			//lws_callback_on_writable( mac->websocket_ptr );
@@ -164,6 +165,7 @@ int WebsocketNotificationsSinkCallback( struct lws *wsi, enum lws_callback_reaso
 				unsigned char *t = e->fq_Data+LWS_SEND_BUFFER_PRE_PADDING;
 				t[ e->fq_Size+1 ] = 0;
 
+				INFO("\t\t\t\t\t\t\t\t\t\t\tSENDMESSSAGE\n<%s> size: %d\n\n\n\n", e->fq_Data+LWS_SEND_BUFFER_PRE_PADDING, e->fq_Size );
 				int res = lws_write( wsi, e->fq_Data+LWS_SEND_BUFFER_PRE_PADDING, e->fq_Size, LWS_WRITE_TEXT );
 				//DEBUG("[websocket_app_callback] message sent: %s len %d\n", e->fq_Data, res );
 
