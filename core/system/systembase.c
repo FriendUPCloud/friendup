57db983f02ba6667763ec979887bb24e09a3a6cd
Friend Core Developer <developer@friendup.cloud>
2019-01-07 12:13:34 +0000

    Debug added

--- core/system/systembase.c
+++ core/system/systembase.c
@@ -2229,7 +2229,7 @@ SQLLibrary *LibrarySQLGet( SystemBase *l )
 			
 				//FRIEND_MUTEX_LOCK( &l->sl_ResourceMutex );
 				retlib = l->sqlpool[l->MsqLlibCounter ].sqllib;
-				DEBUG("retlibptr %p pool %p\n", retlibi, l->sqlpool[l->MsqLlibCounter ].sqllib );
+				DEBUG("retlibptr %p pool %p\n", retlib, l->sqlpool[l->MsqLlibCounter ].sqllib );
 				if( retlib == NULL || retlib->GetStatus( (void *)l->sqlpool[l->MsqLlibCounter ].sqllib ) != SQL_STATUS_READY ) //retlib->con.sql_Con->status != MYSQL_STATUS_READY )
 				{
 					FERROR( "[LibraryMYSQLGet] We found a NULL pointer on slot %d!\n", l->MsqLlibCounter );
