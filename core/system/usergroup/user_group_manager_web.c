e74ba9291ae2e20b208ba9b8f978b88f5457ba57
stefkosdev <ps@friendup.cloud>
2019-01-29 14:23:56 +0100

    @TK-1448 send proper message to external server setusers->update

--- core/system/usergroup/user_group_manager_web.c
+++ core/system/usergroup/user_group_manager_web.c
@@ -636,7 +636,7 @@ Http *UMGWebRequest( void *m, char **urlpath, Http* request, UserSession *logged
 					
 					char msg[ 512 ];
 					snprintf( msg, sizeof(msg), "{\"id\":%lu,\"name\":\"%s\",\"type\":\"%s\"}", fg->ug_ID, fg->ug_Name, fg->ug_Type );
-					NotificationManagerSendEventToConnections( l->sl_NotificationManager, request, NULL, "service", "group", "setusers", msg );
+					NotificationManagerSendEventToConnections( l->sl_NotificationManager, request, NULL, "service", "group", "update", msg );
 
 					char buffer[ 256 ];
 					snprintf( buffer, sizeof(buffer), "ok<!--separate-->{ \"response\": \"sucess\",\"id\":%lu }", fg->ug_ID );
