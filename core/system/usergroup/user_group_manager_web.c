87a449cc0108dd047951779bbe8137204d0d83cf
stefkosdev <ps@friendup.cloud>
2019-01-30 10:09:16 +0100

    @TK-1448 send proper event when usergroup data was changed and proper when users are assigned/changed

--- core/system/usergroup/user_group_manager_web.c
+++ core/system/usergroup/user_group_manager_web.c
@@ -571,6 +571,10 @@ Http *UMGWebRequest( void *m, char **urlpath, Http* request, UserSession *logged
 						l->LibrarySQLDrop( l, sqlLib );
 					}
 					
+					char msg[ 512 ];
+					snprintf( msg, sizeof(msg), "{\"id\":%lu,\"name\":\"%s\",\"type\":\"%s\",\"parentid\":%lu}", fg->ug_ID, fg->ug_Name, fg->ug_Type, fg->ug_ParentID );
+					NotificationManagerSendEventToConnections( l->sl_NotificationManager, request, NULL, "service", "group", "update", msg );
+					
 					// if users parameter is passed then we must remove current users from group
 					if( users != NULL )
 					{
@@ -632,12 +636,10 @@ Http *UMGWebRequest( void *m, char **urlpath, Http* request, UserSession *logged
 							UGMAddUserToGroupDB( l->sl_UGM, groupID, rmEntry->i_Data );
 							FFree( rmEntry );
 						}
+						
+						NotificationManagerSendEventToConnections( l->sl_NotificationManager, request, NULL, "service", "group", "setusers", msg );
 					}
 					
-					char msg[ 512 ];
-					snprintf( msg, sizeof(msg), "{\"id\":%lu,\"name\":\"%s\",\"type\":\"%s\",\"parentid\":%lu}", fg->ug_ID, fg->ug_Name, fg->ug_Type, fg->ug_ParentID );
-					NotificationManagerSendEventToConnections( l->sl_NotificationManager, request, NULL, "service", "group", "update", msg );
-
 					char buffer[ 256 ];
 					snprintf( buffer, sizeof(buffer), "ok<!--separate-->{ \"response\": \"sucess\",\"id\":%lu }", fg->ug_ID );
 					HttpAddTextContent( response, buffer );
