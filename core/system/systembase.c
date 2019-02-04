9edee516e8e855ac2b1937a3495273d164cc5899
stefkosdev <ps@friendup.cloud>
2019-02-04 16:57:15 +0100

    User proper mutex to lock when

--- core/system/systembase.c
+++ core/system/systembase.c
@@ -2488,12 +2488,14 @@ int WebSocketSendMessage( SystemBase *l __attribute__((unused)), UserSession *us
 
 			if( FRIEND_MUTEX_LOCK( &(l->sl_USM->usm_Mutex) ) == 0 )
 			{
+				
 				WebsocketServerClient *wsc = usersession->us_WSClients;
 				while( wsc != NULL )
 				{
 					DEBUG("[SystemBase] Writing to websockets, pointer to ws %p\n", wsc->wsc_Wsi );
 
-					if( FRIEND_MUTEX_LOCK( &(usersession->us_Mutex) ) == 0 )
+					if( FRIEND_MUTEX_LOCK( &(wsc->wsc_Mutex) ) == 0 )
+					//if( FRIEND_MUTEX_LOCK( &(usersession->us_Mutex) ) == 0 )
 					{
 						if( wsc->wsc_Wsi != NULL )
 						{
@@ -2504,7 +2506,8 @@ int WebSocketSendMessage( SystemBase *l __attribute__((unused)), UserSession *us
 							FERROR("Cannot write to WS, WSI is NULL!\n");
 						}
 
-						FRIEND_MUTEX_UNLOCK( &(usersession->us_Mutex) );
+						//FRIEND_MUTEX_UNLOCK( &(usersession->us_Mutex) );
+						FRIEND_MUTEX_UNLOCK( &(wsc->wsc_Mutex) );
 					}
 
 					wsc = (WebsocketServerClient *)wsc->node.mln_Succ;
