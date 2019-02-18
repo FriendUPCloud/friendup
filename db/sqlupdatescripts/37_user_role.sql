d4bec5ab5e47d38180b1e1bd942522773ed3ca52
Hogne Titlestad <ht@friendup.cloud>
2019-02-18 12:26:16 +0100

    Added user role table.

---
+++ db/sqlupdatescripts/37_user_role.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `FUserRolePermission` ( 
+	`ID` bigint(20) NOT NULL AUTO_INCREMENT,
+	`Permission` varchar(32) NOT NULL,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
