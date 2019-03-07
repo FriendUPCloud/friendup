5e27f1bc64d9311911b27fbd80932b30748322b8
Hogne Titlestad <ht@friendup.cloud>
2019-03-07 12:36:51 +0100

    View state doesnt need to complain.

--- interfaces/web_desktop/js/gui/workspace_inside.js
+++ interfaces/web_desktop/js/gui/workspace_inside.js
@@ -8026,21 +8026,21 @@ body .View.Active.IconWindow ::-webkit-scrollbar-thumb
 		// Don't update if not changed
 		if( this.currentViewState == newState ) return;
 		
-		mobileDebug( 'Starting update view state.' + newState, true );
+		//mobileDebug( 'Starting update view state.' + newState, true );
 		
 		if( newState == 'active' )
 		{
 			document.body.classList.add( 'ViewStateActive' );
 			if( isMobile )
 			{
-				mobileDebug( 'Trying to init websocket.' );
+				//mobileDebug( 'Trying to init websocket.' );
 				Workspace.initWebSocket();
 
 				var dl = new FriendLibrary( 'system.library' );
 				dl.addVar( 'status', 0 );
 				dl.onExecuted = function(e,d)
 				{
-					mobileDebug( 'setwsstate active: ' + e );
+					//mobileDebug( 'setwsstate active: ' + e );
 				};
 				dl.execute( 'mobile/setwsstate' );
 			}
@@ -8054,7 +8054,7 @@ body .View.Active.IconWindow ::-webkit-scrollbar-thumb
 				dl.addVar( 'status', 1 );
 				dl.onExecuted = function(e,d)
 				{
-					mobileDebug( 'setwsstate inactive: ' + e );
+					//mobileDebug( 'setwsstate inactive: ' + e );
 				};
 				dl.execute( 'mobile/setwsstate' );
 			}
