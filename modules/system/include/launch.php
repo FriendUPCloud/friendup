825937d08bae2ee91ab36aa47897285e56c5f2f0
Hogne Titlestad <ht@friendup.cloud>
2019-03-07 13:12:16 +0100

    Prepare to enable application permissions.

--- modules/system/include/launch.php
+++ modules/system/include/launch.php
@@ -99,6 +99,13 @@ if( $app->ID )
 		<script src="/webclient/js/apps/api.js"></script>' . $scripts . '
 		<script>
 			' . $scrp . '
+			Application.checkAppPermission = function( key )
+			{
+				var permissions = {}; // <- inject user specific permissions here
+				if( permissions[ key ] )
+					return permissions[ key ];
+				return false;
+			}
 		</script>
 	</head>
 	<body>
