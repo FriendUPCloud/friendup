53ca87932ee203da90f635fcbac3b343fd8f957d
Thomas Wollburg <tw@friendup.cloud>
2019-03-26 14:50:05 +0100

    friend.php adapted to the new handling of large requests from FriendCore where FC will put the data inot a tmp file and pass the path to php and let it extract it there

--- php/friend.php
+++ php/friend.php
@@ -320,10 +320,25 @@ function FindAppInSearchPaths( $app )
 // Get arguments from argv
 if( isset( $argv ) && isset( $argv[1] ) )
 {
+	
+	
+	//die(print_r($argv,1));
+	
 	if( $args = explode( '&', $argv[1] ) )
 	{
 		//include_once( 'classes/logger.php' );
-		//$Logger->log( 'Here are the received args: ' . $argv[1]  . print_r( $args, 1 ) );
+		//$Logger->log( 'Here are the received args: ' . $argv[1]  .' __ ' . count( $args ) . '::' . print_r( $args, 1 ) );
+
+		/*
+			special case for large amount of data in request; Friend Core creates a file for us to read and parse here
+		*/
+		if( count( $args ) == 1 && array_shift( explode('=',$args[0]) ) == 'friendrequestparameters')
+		{
+			$dataset = file_get_contents( end( explode( '=' , $args[0] ) ) );
+			$args = explode( '&', $dataset );
+			//$Logger->log( 'Date from that file: ' . print_r($newargs,1) );
+		}
+
 		$num = 0;
 		$kvdata = new stdClass();
 		foreach ( $args as $arg )
@@ -367,10 +382,10 @@ if( defined( 'FRIEND_USERNAME' ) && defined( 'FRIEND_PASSWORD' ) )
 // No sessionid!!
 if( !$UserAccount && !isset( $groupSession ) && !isset( $GLOBALS[ 'args' ]->sessionid ) && !isset( $GLOBALS[ 'args' ]->authid ) )
 {
-	die( '404' );
+	die( '404 NO SEESION' );
 }
 if( !$UserAccount && isset( $GLOBALS[ 'args' ]->sessionid ) && $GLOBALS[ 'args' ]->sessionid == '(null)' )
-	die( '404' );
+	die( '404 NO SESSION 2' );
 
 // Setup mysql abstraction
 if( file_exists( 'cfg/cfg.ini' ) )
