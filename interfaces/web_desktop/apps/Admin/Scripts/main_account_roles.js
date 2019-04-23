b0785273568c31f16cdbe982a774658cbb7b363d
Hogne Titlestad <ht@friendup.cloud>
2019-04-23 12:41:02 +0200

    Tuning theme to fit GUI in special case.

--- interfaces/web_desktop/apps/Admin/Scripts/main_account_roles.js
+++ interfaces/web_desktop/apps/Admin/Scripts/main_account_roles.js
@@ -584,7 +584,7 @@ Sections.accounts_roles = function( cmd, extra )
 		var d = document.createElement( 'div' );
 		d.className = 'PaddingSmall HContent' + '10' + ' TextCenter FloatLeft Ellipsis';
 		
-		d.innerHTML = '<strong>(+)</strong>';
+		d.innerHTML = '<button type="button" class="FullWidth IconSmall fa-plus NoBorders NoPadding IconButton Negative"> </button>';
 		
 		//d.innerHTML = '<button class="IconButton IconSmall ButtonSmall fa-plus-circle"></button>';
 		
