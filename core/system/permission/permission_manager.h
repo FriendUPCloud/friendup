eb0fca8fcb0ceb87a24012e58233665baf137edf
stefkosdev <ps@friendup.cloud>
2019-05-09 14:46:38 +0200

    @TK-1543 Check Permission functions in progress

---
+++ core/system/permission/permission_manager.h
@@ -0,0 +1,46 @@
+/*©mit**************************************************************************
+*                                                                              *
+* This file is part of FRIEND UNIFYING PLATFORM.                               *
+* Copyright (c) Friend Software Labs AS. All rights reserved.                  *
+*                                                                              *
+* Licensed under the Source EULA. Please refer to the copy of the MIT License, *
+* found in the file license_mit.txt.                                           *
+*                                                                              *
+*****************************************************************************©*/
+/** @file
+ * 
+ *  Permission Manager header
+ *
+ *  @author PS (Pawel Stefanski)
+ *  @date created 09/05/2019
+ */
+
+#ifndef __SYSTEM_PERMISSION_PERMISSION_MANAGER_H__
+#define __SYSTEM_PERMISSION_PERMISSION_MANAGER_H__
+
+#include <core/types.h>
+#include <core/nodes.h>
+#include <system/dictionary/dictionary.h>
+
+//
+// Permission Manager
+//
+
+typedef struct PermissionManager
+{
+	void						*pm_SB;
+	pthread_mutex_t				pm_Mutex;
+}PermissionManager;
+
+//
+
+PermissionManager *PermissionManagerNew( void *sb );
+
+void PermissionManagerDelete( PermissionManager *pm );
+
+FBOOL PermissionManagerCheckAppPermission( PermissionManager *pm, char *key, char *appname );
+
+FBOOL PermissionManagerCheckPermission( PermissionManager *pm, char *type, char *identifier );
+
+#endif //__SYSTEM_PERMISSION_PERMISSION_MANAGER_H__
+
