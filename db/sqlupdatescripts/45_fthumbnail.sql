6e75395a22dfc13d0b3104a527e660dcc20827dd
Hogne Titlestad <ht@friendup.cloud>
2019-07-03 16:44:28 +0200

    Better table structure.

--- db/sqlupdatescripts/45_fthumbnail.sql
+++ db/sqlupdatescripts/45_fthumbnail.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `FThumbnail` (
   `Path` varchar(255) NOT NULL,
   `Filepath` varchar(255) NOT NULL,
   `DateCreated` datetime,
+  `DateTouched` datetime,
   `Encryption` varchar(255),
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
