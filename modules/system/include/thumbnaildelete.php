ad4266b983d3180e46a5a6cf5efba57da74e2477
Hogne Titlestad <ht@friendup.cloud>
2019-07-03 16:51:05 +0200

    Missing file for deleting thumbnails.

---
+++ modules/system/include/thumbnaildelete.php
@@ -0,0 +1,16 @@
+<?php
+
+/*©lgpl*************************************************************************
+*                                                                              *
+* This file is part of FRIEND UNIFYING PLATFORM.                               *
+* Copyright (c) Friend Software Labs AS. All rights reserved.                  *
+*                                                                              *
+* Licensed under the Source EULA. Please refer to the copy of the GNU Lesser   *
+* General Public License, found in the file license_lgpl.txt.                  *
+*                                                                              *
+*****************************************************************************©*/
+
+// Fail
+die( 'fail<!--separate-->{"message":"Could not delete thumbnail file by path.","response":-1}' );
+
+?>
