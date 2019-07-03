a46c453b7c2c047288d8ab21f1076e82ef48a950
stefkosdev <ps@friendup.cloud>
2019-07-03 15:20:43 +0200

    SambaFS enabled again

--- core/system/fsysdyn/fsyssmb.c
+++ core/system/fsysdyn/fsyssmb.c
@@ -256,7 +256,7 @@ void deinit( struct FHandler *s )
 void *Mount( struct FHandler *s, struct TagItem *ti, UserSession *usrs, char **mountError )
 {
 	FERROR("Disabled for a moment\n");
-	return NULL;
+	//return NULL;
 	
 	File *dev = NULL;
 	char *path = NULL, *ulogin = NULL, *upass = NULL;
