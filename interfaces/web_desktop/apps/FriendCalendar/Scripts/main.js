2238bf2ea68a8ceddd09d897523ba34a7a4d01d6
Hogne Titlestad <ht@friendup.cloud>
2019-07-06 15:11:48 +0200

    Cosmetics. Now we can select event ranges.

--- interfaces/web_desktop/apps/FriendCalendar/Scripts/main.js
+++ interfaces/web_desktop/apps/FriendCalendar/Scripts/main.js
@@ -20,6 +20,22 @@ var eventPaletteForeground = [
 	'#ffffff'
 ];
 
+// Global events ---------------------------------------------------------------
+var moveListener = null;
+var upListener = null;
+
+window.addEventListener( 'mouseup', function( e )
+{
+	if( upListener ) upListener( e );
+} );
+window.addEventListener( 'mousemove', function( e )
+{
+	if( moveListener ) moveListener( e );
+} );
+// End global events -----------------------------------------------------------
+
+var calendarRowHeight = 30; // <- will be overwritten below w actual height
+
 // Get the users
 {
 	var you = document.createElement( 'div' );
@@ -310,13 +326,13 @@ var Calendar = {
 						{
 							var ypos = events[ b ].DateStart.split( ' ' )[1];
 							ypos = ypos.split( ':' );
-							ypos = parseInt( ypos[0] ) + ( ypos[1] / 30 * 0.5 );
+							ypos = parseInt( ypos[0] ) + ( ypos[1] / calendarRowHeight * 0.5 );
 							
 							ypos = ypos / 24 * 100;
 							
 							var height = events[ b ].DateEnd.split( ' ' )[1];
 							height = height.split( ':' );
-							height = parseInt( height[0] ) + ( height[1] / 30 * 0.5 );
+							height = parseInt( height[0] ) + ( height[1] / calendarRowHeight * 0.5 );
 							
 							height = height / 24 * 100;
 							height = height - ypos;
@@ -327,17 +343,6 @@ var Calendar = {
 								height: height,
 								event: events[ b ]
 							} );
-							
-							/*var t = 
-							if( events[ b ].DateStart == cand )
-							{
-								var str = 'top: 0px; left: 0px; width: 100%; height: 10px; background: green;';
-								timez += '<div class="TimeEvent" style="' + st + '">' + events[b].Name + '</div>';
-							}
-							else
-							{
-								console.log( 'What: ' + cand + ' == ' + events[ b ].DateStart, events[ b ] );
-							}*/
 						}
 					}
 					
@@ -359,12 +364,84 @@ var Calendar = {
 		if( calStart )
 			ml += '</div>';
 		
-		// Add events and add element
 		var eventDiv = document.createElement( 'div' );
 		eventDiv.className = 'WeekContainer';
+		// Add events and add element ------------------------------------------
 		eventDiv.addEventListener( 'mousedown', function( e )
 		{
+			// Find day element
+			var t = e.target ? e.target : e.srcElement;
+			while( t != document.body )
+			{
+				if( t.classList && t.classList.contains( 'Day' ) )
+					break;
+				t = t.parentNode;
+			}
+			// Error!
+			if( t == document.body )
+				return;
+			
+			// Correct, displayed height
+			calendarRowHeight = t.querySelector( '.TimeSlot' ).offsetHeight * 0.5;
+			
+			var scrollT = ge( 'MainView' ).querySelector( '.CalendarDates' ).scrollTop;
+			
+			eventDiv.data = {
+				mousedown: true,
+				mode: 0,
+				x: e.clientX,
+				y: e.clientY + scrollT,
+				dayElement: t
+			};
 		} );
+		moveListener = function( e )
+		{
+			if( !eventDiv || !eventDiv.data || !eventDiv.data.mousedown )
+				return;
+			var scrollT = ge( 'MainView' ).querySelector( '.CalendarDates' ).scrollTop;
+			var d = eventDiv.data;
+			var ymotion = ( e.clientY + scrollT ) - d.y;
+			ymotion = Math.floor( ymotion / calendarRowHeight ) * calendarRowHeight;
+			
+			// First clicking
+			if( d.mode == 0 && ymotion > 10 )
+			{
+				// Error!
+				if( !d.dayElement ) return;
+								
+				// Make a new event rect
+				d.mode = 1;
+				if( d.div )
+				{
+					d.div.parentNode.removeChild( d.div );
+					d.div = null;
+				}
+				d.div = document.createElement( 'div' );
+				d.div.className = 'EventRect New';
+				
+				// Add the element
+				d.dayElement.appendChild( d.div );
+				var top = GetElementTop( d.dayElement );
+				d.div.style.top = Math.floor( ( d.y - top ) / calendarRowHeight ) * calendarRowHeight + 'px';
+			}
+			// Resizing mode
+			else if( d.mode == 1 )
+			{
+				if( ymotion > 10 )
+				{
+					d.div.style.height = ymotion + 'px';
+				}
+				else
+				{
+					d.div.style.height = '';
+				}
+			}
+		};
+		upListener = function( e )
+		{
+			eventDiv.data = null;
+		}
+		// Done events ---------------------------------------------------------
 		eventDiv.innerHTML = ml;
 		ge( 'MainView' ).innerHTML = '';
 		ge( 'MainView' ).appendChild( eventDiv );
