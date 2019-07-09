a7d22acb2134d00ef63485b0d9de5a82b6a2bc78
Hogne Titlestad <ht@friendup.cloud>
2019-07-09 11:25:02 +0200

    Apply secure calendar sharing.

--- modules/system/include/calendarshare.php
+++ modules/system/include/calendarshare.php
@@ -9,6 +9,96 @@
 *                                                                              *
 *****************************************************************************©*/
 
+// Get own workgroups
+if( $workgroups = $SqlDatabase->FetchObjects( '
+	SELECT ug.ID FROM FUserGroup ug, FUserToGroup fug 
+	WHERE 
+		    fug.UserID=\'' . $User->ID . '\'
+		AND fug.UserGroupID = ug.ID
+' ) )
+{
+	// Setup vars
+	$ids = [];
+	foreach( $workgroups as $wg )
+	{
+		$ids[] = $wg->ID;
+	}
+	$uids = [];
 
+	// List all users by connection to workgroup
+	if( $users = $SqlDatabase->FetchObjects( '
+		SELECT 
+			u.ID FROM FUser u
+		WHERE
+			u.ID IN ( 
+				SELECT i.UserID FROM FUserToGroup i, FUserGroup ug WHERE ug.Type = "Workgroup" AND ug.ID IN ( ' . implode( ',', $ids ) . ' )
+			)
+	' ) )
+	{
+		foreach( $users as $u )
+		{
+			$uids[] = $u->ID;
+		}
+	}
+	
+	// Final
+	$workgroups = [];
+	$users = [];
+	
+	if( count( $ids ) && count( $args->args->wids ) )
+	{
+		// Confirm that we are part of these workgroups
+		foreach( $args->args->wids as $wid )
+		{
+			if( in_array( $wid, $ids ) )
+			{
+				$workgroups[] = $wid;
+			}
+		}
+	}
+	if( count( $uids ) && count( $args->args->uids ) )
+	{
+		foreach( $args->args->uids as $uid )
+		{
+			if( in_array( $uid, $uids ) )
+			{
+				$users[] = $uid;
+			}
+		}
+	}
+	
+	// Store relation
+	if( count( $users ) )
+	{
+		foreach( $users as $uid )
+		{
+			$d = new dbIO( 'FMetaData' );
+			$d->Key = 'UserUserCalendarRelation';
+			$d->DataID = $User->ID;
+			$d->DataTable = 'FUser';
+			$d->ValueNumber = intval( $uid, 10 );
+			$d->ValueString = 'Default'; // <- Calendar type - TODO: Dynamic!
+			$d->Load();
+			$d->Save();
+		}
+	}
+	if( count( $workgroups ) )
+	{
+		foreach( $workgroups as $wid )
+		{
+			$d = new dbIO( 'FMetaData' );
+			$d->Key = 'UserWorkgroupCalendarRelation';
+			$d->DataID = $User->ID;
+			$d->DataTable = 'FUserGroup';
+			$d->ValueNumber = intval( $wid, 10 );
+			$d->ValueString = 'Default'; // <- Calendar type - TODO: Dynamic!
+			$d->Load();
+			$d->Save();
+		}
+	}
+	die( 'ok<!--separate-->{"response":-1,"message":"Calendar was shared."}' );
+	
+}
+die( 'fail<!--separate-->{"response":-1,"message":"No users or workgroups found."}' );
 
 ?>
