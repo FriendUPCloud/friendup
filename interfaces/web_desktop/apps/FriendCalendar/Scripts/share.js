4fdb0fb38dd73fdbabccf337600cf45da252f491
Hogne Titlestad <ht@friendup.cloud>
2019-07-09 11:08:17 +0200

    Gui ready for executing sharing.

--- interfaces/web_desktop/apps/FriendCalendar/Scripts/share.js
+++ interfaces/web_desktop/apps/FriendCalendar/Scripts/share.js
@@ -32,7 +32,9 @@ Application.run = function( msg )
 				{
 					sw = sw == 1 ? 2 : 1;
 					var r = document.createElement( 'div' );
-					r.className = 'HRow PaddingSmall sw' + sw;
+					r.className = 'HRow rowUser PaddingSmall sw' + sw;
+					r.setAttribute( 'id', usl[ a ].ID );
+					r.setAttribute( 'name', usl[ a ].Fullname );
 					r.innerHTML = '<div class="HContent40 FloatLeft Ellipsis">' + usl[ a ].Fullname + '</div><div class="HContent60 FloatLeft Ellipsis">' + usl[ a ].Name + ( usl[ a ].Email ? ( ', ' + usl[ a ].Email ) : '' ) + '</div>';
 					r.onclick = function()
 					{
@@ -75,7 +77,9 @@ Application.run = function( msg )
 				{
 					sw = sw == 1 ? 2 : 1;
 					var r = document.createElement( 'div' );
-					r.className = 'HRow PaddingSmall sw' + sw;
+					r.className = 'HRow rowWorkgroup PaddingSmall sw' + sw;
+					r.setAttribute( 'id', wl[ a ].ID );
+					r.setAttribute( 'name', wl[ a ].Name );
 					r.innerHTML = '<div class="HContent100 FloatLeft Ellipsis">' + wl[ a ].Name + '</div>';
 					r.onclick = function()
 					{
@@ -113,11 +117,65 @@ function confirmShare()
 	if( eles.length )
 	{
 		ge( 'top' ).classList.add( 'Hide' );
+		
+		ge( 'ShareHeading' ).innerHTML = i18n( 'i18n_confirm_sharing' );
+		
+		var sw = 0;
+		
+		var out = '<div class="List">';
+		for( var a = 0; a < eles.length; a++ )
+		{
+			sw = sw == 1 ? 2 : 1;
+			var name = eles[a].classList.contains( 'rowUser' ) ? i18n( 'i18n_user' ) : i18n( 'i18n_workgroup' );
+			out += '<div class="HRow sw' + sw + ' PaddingSmall">' + name + ': ' + eles[a].getAttribute( 'name' ) + '</div>';
+		}
+		out += '</div>';
+		
+		ge( 'ShareMessage' ).innerHTML = out;
+		
+		
 		ge( 'ShareDone' ).classList.add( 'Show' );
 	}
 	else
 	{
 		Alert( i18n( 'i18n_please_select' ), i18n( 'i18n_please_select_desc' ) );
+	}
+}
+
+// Ok, now we are ready to go
+function FinalShare()
+{
+	var eles = ge( 'top' ).getElementsByClassName( 'Selected' );
+	if( eles.length )
+	{
+		var wids = [];
+		var uids = [];
+		for( var a = 0; a < eles.length; a++ )
+		{
+			if( eles[a].classList.contains( 'rowWorkgroup' ) )
+				wids.push( eles[a].getAttribute( 'id' ) );
+			else if( eles[a].classList.contains( 'rowUser' ) )
+				uids.push( eles[a].getAttribute( 'id' ) );
+		}
+		if( wids.length > 0 || uids.length > 0 )
+		{
+			var m = new Module( 'system' );
+			m.onExecuted = function( e, d )
+			{
+				if( e != 'ok' )
+				{
+					Alert( i18n( 'i18n_error_occured' ), i18n( 'i18n_could_not_share' ) );
+					return false;
+				}
+				Alert( i18n( 'i18n_calendar_shared' ), i18n( 'i18n_calendar_shared_desc' ) );
+				CloseWindow();
+			}
+			m.execute( 'calendarshare', { uids: uids, wids: wids } );
+		}
+		else
+		{
+			confirmShare();
+		}
 	}
 }
 
