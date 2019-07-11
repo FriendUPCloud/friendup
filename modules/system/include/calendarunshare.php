145993a1b6c8c511194b160da777995be65243e6
Hogne Titlestad <ht@friendup.cloud>
2019-07-11 11:44:18 +0200

    Unsharing now works.

--- modules/system/include/calendarunshare.php
+++ modules/system/include/calendarunshare.php
@@ -9,7 +9,70 @@
 *                                                                              *
 *****************************************************************************©*/
 
+global $SqlDatabase, $User, $Logger;
 
+if( isset( $args->args ) )
+{
+	$unshareCount = 0;
+	//$Logger->log( '[calendarunshare] We got args.' );
+	foreach( $args->args as $calName => $cal )
+	{
+		$usc = $wsc = false;
+		//$Logger->log( '[calendarunshare]  * Calendar ' . $calName );
+		if( isset( $cal->users ) )
+		{
+			// Sanitize
+			foreach( $cal->users as $k=>$v )
+				$cal->users{$k} = intval( $v, 10 );
+			
+			$SqlDatabase->query( $q = '
+				DELETE FROM FMetaData
+				WHERE 
+					`Key` = \'UserUserCalendarRelation\' AND
+					DataID = \'' . $User->ID . '\' AND
+					DataTable = \'FUser\' AND
+					ValueString = \'' . $calName . '\' AND
+					ValueNumber IN ( ' . implode( ', ', $cal->users ) . ' )
+			' );
+			
+			$wsc = true;
+			//$Logger->log( '[calendarunshare]     Users removed; ' . implode( ', ', $cal->users ) );
+			//$Logger->log( $q );
+		}
+		if( isset( $cal->workgroups ) )
+		{
+			// Sanitize
+			foreach( $cal->workgroups as $k=>$v )
+				$cal->workgroups{$k} = intval( $v, 10 );
+			
+			$SqlDatabase->query( '
+				DELETE FROM FMetaData
+				WHERE 
+					`Key` = \'UserWorkgroupCalendarRelation\' AND
+					DataID = \'' . $User->ID . '\' AND
+					DataTable = \'FUser\' AND
+					ValueString = \'' . $calName . '\' AND
+					ValueNumber IN ( ' . implode( ', ', $cal->workgroups ) . ' )
+			' );
+			
+			$usc = true;
+			//$Logger->log( '[calendarunshare]     Workgroups removed; ' . implode( ', ', $cal->workgroups ) );
+		}
+		
+		if( $usc || $wsc )
+			$unshareCount++;
+	}
+	
+	// Successful?
+	if( $unshareCount > 0 )
+	{
+		//$Logger->log( '[calendarunshare]  Successful unsharing..' );
+		die( 'ok<!--separate-->{"response":1,"message":"Successfully unshared specified calendars."}' );
+	}
+	
+}
+
+die( 'fail<!--separate-->{"response":-1,"message":"Failed to unshare unspecified calendars."}' );
 
 
 ?>
