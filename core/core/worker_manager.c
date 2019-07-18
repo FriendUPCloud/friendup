e4d49a87eee7a96d2b2d4a525419bdd49e3b77ee
stefkosdev <ps@friendup.cloud>
2019-07-18 08:22:03 +0200

    AS locking added to WorkerDebug function Check directory lock before new one is created

--- core/core/worker_manager.c
+++ core/core/worker_manager.c
@@ -299,7 +299,8 @@ int WorkerManagerRun( WorkerManager *wm,  void (*foo)( void *), void *d, void *w
 	return 0;
 }
 
-/**
+/*
+*
 * For debug
 *
 */
@@ -315,10 +316,14 @@ void WorkerManagerDebug( void *sb )
 		{
 			if( wm->wm_Workers[ i ] != NULL )
 			{
-				Http *request = (Http *)wm->wm_Workers[ i ]->w_Request;
-				if( request != NULL )
+				if( FRIEND_MUTEX_LOCK( &(wm->wm_Workers[ i ]->w_Mut) ) == 0 )
 				{
-					Log( FLOG_ERROR, "[WorkerManager] worker: %d content: %s pointer to session: %p rawrequest: %s\n", i, request->content, request->h_UserSession, request->rawRequestPath );
+					Http *request = (Http *)wm->wm_Workers[ i ]->w_Request;
+					if( request != NULL )
+					{
+						Log( FLOG_ERROR, "[WorkerManager] worker: %d content: %s pointer to session: %p rawrequest: %s\n", i, request->content, request->h_UserSession, request->rawRequestPath );
+					}
+					FRIEND_MUTEX_UNLOCK( &(wm->wm_Workers[ i ]->w_Mut) );
 				}
 			}
 		}
