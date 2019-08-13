55d9ca163b7e4bbd8ff975b2e69518c51876ee73
Hogne Titlestad <ht@friendup.cloud>
2019-08-13 09:29:45 +0200

    More test1

--- interfaces/web_desktop/js/workspace.js
+++ interfaces/web_desktop/js/workspace.js
@@ -793,7 +793,7 @@ Workspace = {
 			}
 			else
 			{
-				console.log( 'Wait a second before you can log in again.' );
+				console.log( 'Test1: Wait a second before you can log in again.' );
 				// Wait a second before trying again
 				setTimeout( function()
 				{
