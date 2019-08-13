38fa439b58055102737e42d44ed20e593abffed2
Hogne Titlestad <ht@friendup.cloud>
2019-08-13 12:11:30 +0200

    More debug

--- interfaces/web_desktop/js/io/cajax.js
+++ interfaces/web_desktop/js/io/cajax.js
@@ -582,6 +582,7 @@ cAjax.prototype.send = function( data )
 	// TODO: Check that the websocket actually is OPEN (Chrome being silly)
 	if( self.mode == 'websocket' && Workspace.conn && Workspace.conn.ws )
 	{
+        console.log( 'Test2: Sending with websockets.' );
         var u = self.url.split( '?' );
         var wsdata = ( data ? data : {} );
         if( self.vars )
@@ -665,6 +666,7 @@ cAjax.prototype.send = function( data )
 				for( var a in this.vars )
 					out.push( a + '=' + this.vars[a] );
 				res = this.proxy.send( out.join ( '&' ) );
+				console.log( 'Test2: Here u: ' + out.join( '&' ) );
 			}
 			// All else fails?
 			else
@@ -681,6 +683,7 @@ cAjax.prototype.send = function( data )
 			var u = this.url.split( '?' );
 			u = u[0] + '?' + ( u[1] ? ( u[1]+'&' ) : '' ) + 'cachekiller=' + this.getRandNumbers();
 			this.proxy.setRequestHeader( 'Method', 'GET ' + u + ' HTTP/1.1' );
+			console.log( 'Test2: Here: ' + u );
 			try 
 			{ 
 				res = this.proxy.send( null ); 
