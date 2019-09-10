06a243cf86b33fc75694cca057b70eb2c4bbbb87
Hogne Titlestad <ht@friendup.cloud>
2019-09-10 13:23:08 +0200

    Fix mouse move issue.

--- interfaces/web_desktop/js/gui/guibase.js
+++ interfaces/web_desktop/js/gui/guibase.js
@@ -690,7 +690,7 @@ var mousePointer =
 				this.dom.parentNode.removeChild ( this.dom );
 			this.dom = false;
 		}
-		else
+		else if ( this.dom && this.dom.firstChild )
 		{
 			this.dom.style.top = windowMouseY - ( this.dom.firstChild.offsetHeight >> 1 ) + 'px';
 			this.dom.style.left = windowMouseX - ( this.dom.firstChild.offsetWidth >> 1 ) + 'px';
