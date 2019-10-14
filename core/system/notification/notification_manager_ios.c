6a3a8b438075ce8d579fd3e13f74a5ebc1364410
stefkosdev <ps@friendup.cloud>
2019-10-14 11:54:48 +0200

    Changed TLS to DTLS Store information about fd's in files Code cleanup

--- core/system/notification/notification_manager_ios.c
+++ core/system/notification/notification_manager_ios.c
@@ -202,7 +202,8 @@ int NotificationManagerNotificationSendIOS( NotificationManager *nm, const char 
 	SSLeay_add_ssl_algorithms();
 	SSL_load_error_strings();
 	
-	ctx = SSL_CTX_new(TLSv1_2_method());
+	ctx = SSL_CTX_new( DTLS_client_method() );
+	//ctx = SSL_CTX_new(TLSv1_2_method());
 	//ctx = SSL_CTX_new(TLSv1_method());
 	if( !ctx )
 	{
@@ -390,7 +391,8 @@ void NotificationIOSSendingThread( FThread *data )
 	SSLeay_add_ssl_algorithms();
 	SSL_load_error_strings();
 	
-	ctx = SSL_CTX_new(TLSv1_2_method());
+	ctx = SSL_CTX_new( DTLS_client_method() );
+	//ctx = SSL_CTX_new(TLSv1_2_method());
 	//ctx = SSL_CTX_new(TLSv1_method());
 	if( !ctx )
 	{
