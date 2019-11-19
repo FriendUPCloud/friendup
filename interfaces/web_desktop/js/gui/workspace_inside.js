7454805ab37845148f42459e3b639b44b14e2497
Hogne Titlestad <ht@friendup.cloud>
2019-11-19 15:46:54 +0100

    Trap all switches

--- interfaces/web_desktop/js/gui/workspace_inside.js
+++ interfaces/web_desktop/js/gui/workspace_inside.js
@@ -9527,12 +9527,14 @@ Workspace.receivePush = function( jsonMsg )
 			if( !app )
 			{
 				Notify( { title: i18n( 'i18n_could_not_find_application' ), text: i18n( 'i18n_could_not_find_app_desc' ) } );
+				if( Workspace.onReady ) Workspace.onReady();
 				return;
 			}
 		
 			if( !app.contentWindow ) 
 			{
 				Notify( { title: i18n( 'i18n_could_not_find_application' ), text: i18n( 'i18n_could_not_find_app_desc' ) } );
+				if( Workspace.onReady ) Workspace.onReady();
 				return;
 			}
 		
