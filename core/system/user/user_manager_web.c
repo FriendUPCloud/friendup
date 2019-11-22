fc5386fe8d67976fe3b98b3ad303ba311db90a16
stefkosdev <ps@friendup.cloud>
2019-11-22 14:21:59 +0100

    Debug removed. Fixed notification

--- core/system/user/user_manager_web.c
+++ core/system/user/user_manager_web.c
@@ -37,13 +37,13 @@ inline static void NotifyExtServices( SystemBase *l, Http *request, User *usr )
 	msize = snprintf( msg, sizeof(msg), "{\"userid\":\"%s\",\"lastupdate\":%lu,\"groups\":[", usr->u_UUID, usr->u_ModifyTime );
 
 	BufStringAddSize( bs, msg, msize );
-	DEBUG("NotifyExtServices1: %s\n", bs->bs_Buffer );
+	//DEBUG("NotifyExtServices1: %s\n", bs->bs_Buffer );
 	UGMGetUserGroupsDB( l->sl_UGM, usr->u_ID, bs );
-	DEBUG("NotifyExtServices2: %s\n", bs->bs_Buffer );
+	//DEBUG("NotifyExtServices2: %s\n", bs->bs_Buffer );
 	BufStringAddSize( bs, "]}", 2 );
-	DEBUG("NotifyExtServices3: %s\n", bs->bs_Buffer );
+	//DEBUG("NotifyExtServices3: %s\n", bs->bs_Buffer );
 	
-	NotificationManagerSendEventToConnections( l->sl_NotificationManager, request, NULL, NULL, "service", "user", "update", msg );
+	NotificationManagerSendEventToConnections( l->sl_NotificationManager, request, NULL, NULL, "service", "user", "update", bs->bs_Buffer );
 	
 	BufStringDelete( bs );
 }
