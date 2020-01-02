26b6151dd2a2609587ef2f4451d321d8a4624d9a
stefkosdev <ps@friendup.cloud>
2020-01-02 10:53:42 +0100

    FIX - rename in localfs

--- core/system/fsysdyn/fsyslocal.c
+++ core/system/fsysdyn/fsyslocal.c
@@ -829,24 +829,39 @@ int Rename( struct File *s, const char *path, const char *nname )
 {
 	DEBUG("Rename!\n");
 	char *newname = NULL;
+	
+	// remove disk name
+	char *pathNoDiskName = (char *)path;
 	int spath = strlen( path );
+	
+	int i;
+	for( i = 0 ; i < spath ; i++ )
+	{
+		if( path[ i ] == ':' )
+		{
+			pathNoDiskName = (char *)&(path[ i+1 ]);
+			spath -= i+1;
+			break;
+		}
+	}
+	
 	int rspath = strlen( s->f_Path );
 	
 	// 1a. is the source a folder? If so, remove trailing /
 	char *targetPath = NULL;
 	
-	if( path[spath-1] == '/' )
+	if( pathNoDiskName[spath-1] == '/' )
 	{
 		targetPath = FCalloc( spath, sizeof( char ) );
-		sprintf( targetPath, "%.*s", spath - 1, path );
+		sprintf( targetPath, "%.*s", spath - 1, pathNoDiskName );
 	}
 	else
 	{
 		targetPath = FCalloc( spath + 1, sizeof( char ) ); 
-		sprintf( targetPath, "%.*s", spath, path );
+		sprintf( targetPath, "%.*s", spath, pathNoDiskName );
 	}
 	
-	// 1b. Do we have a sub folder in path?
+	// 1b. Do we have a sub folder in pathNoDiskName?
 	int hasSubFolder = 0;
 	int off = 0;
 	int c = 0; for( ; c < spath; c++ )
