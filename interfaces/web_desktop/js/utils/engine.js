29dd9242687d7717ec20f919eee7aae0eeb94e10
Hogne Titlestad <ht@friendup.cloud>
2020-01-02 23:04:55 +0100

    Fix problem with not activating a tab.

--- interfaces/web_desktop/js/utils/engine.js
+++ interfaces/web_desktop/js/utils/engine.js
@@ -2162,8 +2162,7 @@ function InitTabs( pdiv, tabCallback )
 			divs[a].onclick = function()
 			{
 				// Already active? Just return
-				if( this.classList.contains( 'TabActive' ) ) return;
-				
+				if( this.classList.contains( 'TabActive' ) && this.pages[ this.index ].classList.contains( 'PageActive' ) ) return;
 				// Assume it is ok to activate this tab
 				var result = true;
 				
@@ -2419,13 +2418,8 @@ function InitTabs( pdiv, tabCallback )
 	}
 	if( tabs.length && tabs[active] )
 	{
-		tabs[active].onclick();
+		tabs[active].click();
 	}
-	
-	// We need an active page
-	var pa = pdiv.getElementsByClassName( 'Page' );
-	if( pa.length == 1 )
-		pa[0].classList.add( 'PageActive' );
 }
 
 // Double click simulator for youch
