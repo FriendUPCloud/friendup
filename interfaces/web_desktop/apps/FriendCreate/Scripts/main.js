a3638553a17e803d296bb4e5deb81171911aa4ad
Hogne Titlestad <ht@friendup.cloud>
2020-01-11 16:50:13 +0100

    Test.

--- interfaces/web_desktop/apps/FriendCreate/Scripts/main.js
+++ interfaces/web_desktop/apps/FriendCreate/Scripts/main.js
@@ -1584,6 +1584,14 @@ function RefreshProjects()
 		
 		for( var a in list )
 		{
+			var paths = '';
+			if( list[a].path.indexOf( '/' ) > 0 )
+			{
+				var p = list[a].path.split( '/' );
+				for( var z = 0; z < depth; z++ )
+					paths += p[ z ] + '/';
+			}
+			
 			// This is a file item
 			if( list[ a ].levels.length == depth )
 			{
@@ -1603,19 +1611,18 @@ function RefreshProjects()
 				
 				var cl = projectFolders[ projectId ][ list[ a ].path ] && projectFolders[ projectId ][ list[ a ].path ].state == 'open' ? ' Open' : '';
 				str += '<li class="Folder ' + cl + '" path="' + list[a].path + '" projectId="' + projectId + '" onclick="SetCurrentProject( \'' + projectId + '\' ); ToggleOpenFolder(this); cancelBubble( event )">' + list[ a ].levels[ depth - 1 ] + '/</li>';
-				str += listFiles( list, depth + 1, list[ a ].path, projectId );
+				
+				if( !listedFolders[ projectId ][ paths ] )
+				{
+					listedFolders[ projectId ][ paths ] = true;
+					str += listFiles( list, depth + 1, list[ a ].path, projectId );
+				}
 			}
 			// This is a folder that may contain a folder
 			else if( list[a].path.indexOf( '/' ) > 0 )
 			{
-				var p = list[a].path.split( '/' );
-				var paths = '';
-				for( var z = 0; z < depth; z++ )
-					paths += p[ z ] + '/';
-				
 				if( !listedFolders[ projectId ][ paths ] )
 				{
-				
 					listedFolders[ projectId ][ paths ] = true;
 				
 					if( !projectFolders[ projectId ][ paths ] )
