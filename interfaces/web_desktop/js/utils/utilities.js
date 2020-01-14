5b38794341e0782381759544eca05c703bcb7b76
Thomas Wollburg <tw@friendup.cloud>
2020-01-14 12:47:37 +0100

    Upload changed to allow multiple files + update its content after file selection Also set to be close on successful upload

--- interfaces/web_desktop/js/utils/utilities.js
+++ interfaces/web_desktop/js/utils/utilities.js
@@ -21,6 +21,24 @@ Friend = window.Friend || {};
 Friend.Utilities = Friend.Utilities || {};
 
 
+Friend.Utilities.humanFileSize = function(bytes, si) {
+	if( si !== false ) si = true;
+	
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
+
 // APIDIRECTCODESTART
 Friend.Utilities.getClass = function( source )
 {
