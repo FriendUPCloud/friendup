099a0ed6229bd5be6c57afc19d9d9aff66334b0f
stefkosdev <ps@friendup.cloud>
2020-01-20 10:38:45 +0100

    Performance flag added

--- core/network/protocol_websocket.c
+++ core/network/protocol_websocket.c
@@ -1035,8 +1035,8 @@ int FC_Callback( struct lws *wsi, enum lws_callback_reasons reason, void *user, 
 
 					lws_write( wsi, e->fq_Data+LWS_SEND_BUFFER_PRE_PADDING, e->fq_Size, LWS_WRITE_TEXT );
 					
-#ifdef __DEBUG__
-					DEBUG("PERFCHECK: Websocket message sent time: %f\n", ((GetCurrentTimestampD()-e->fq_stime)) );
+#ifdef __PERF_MEAS
+					Log( FLOG_INFO, "PERFCHECK: Websocket message sent time: %f\n", ((GetCurrentTimestampD()-e->fq_stime)) );
 #endif
 
 					int errret = lws_send_pipe_choked( wsi );
