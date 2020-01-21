4f144a4c6ff6d3075b60cdc768fa6842e6117db7
Hogne Titlestad <ht@friendup.cloud>
2020-01-21 14:41:32 +0100

    Some socket improvements and comments for speed.

--- core/network/socket.c
+++ core/network/socket.c
@@ -1466,7 +1466,7 @@ inline int SocketRead( Socket* sock, char* data, unsigned int length, unsigned i
 		struct timeval timeout;
 		fd_set fds;
 #define MINIMUMRETRY 30000
-		int retryCount = expectedLength > 0 ? MINIMUMRETRY : 1500;
+		int retryCount = expectedLength > 0 ? MINIMUMRETRY : 1500; // User do be 3000
 		if( expectedLength > 0 && length > expectedLength ) length = expectedLength;
 		int startTime = time( NULL );
 
@@ -1508,9 +1508,10 @@ inline int SocketRead( Socket* sock, char* data, unsigned int length, unsigned i
 					// NB: We used to retry 10000 times!
 					if( read == 0 && read_retries++ < retryCount )
 					{
-						usleep( 1 );
+						usleep( 1 )
 						// We are downloading a big file
 
+						// TODO: This usleep is the old code (before usleep(1))
 						//usleep( read_retries < 100 ? 0 : ( retryCount << 1 ) );
 
 						/*int blocked = sock->s_Blocked;
