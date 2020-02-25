10d849a555447523f49273bd6f5d380246ab198e
stefkosdev <ps@friendup.cloud>
2020-02-25 14:21:40 +0100

    Added new group of calls "service"

--- core/system/services/services_manager_web.c
+++ core/system/services/services_manager_web.c
@@ -45,7 +45,7 @@ typedef struct SServ
  * @return reponse in Http structure
  */
 
-Http *ServiceManagerWebRequest( void *lsb, char **urlpath, Http* request, UserSession *loggedSession )
+Http *ServicesManagerWebRequest( void *lsb, char **urlpath, Http* request, UserSession *loggedSession )
 {
 	SystemBase *l = (SystemBase *)lsb;
 	char *serviceName = NULL;
@@ -698,96 +698,6 @@ Http *ServiceManagerWebRequest( void *lsb, char **urlpath, Http* request, UserSe
 			char buffer[ 256 ];
 			snprintf( buffer, sizeof(buffer), "fail<!--separate-->{ \"response\": \"%s\", \"code\":\"%d\" }", l->sl_Dictionary->d_Msg[DICT_ADMIN_RIGHT_REQUIRED] , DICT_ADMIN_RIGHT_REQUIRED );
 			HttpAddTextContent( response, buffer );
-		}
-	}
-	
-	/// @cond WEB_CALL_DOCUMENTATION
-	/**
-	*
-	* <HR><H2>system.library/service/request</H2>Notify external connections
-	*
-	* @param sessionid - (required) session id of logged user
-	* @param params - (required) paramaters
-	* @param type - (required) message type
-	* @param group - (required) group of message
-	* @param action - (required) action
-	* @param servername - name of the server to which message will be send or put NULL if to all
-	* @return { result: 0 } when success, otherwise error with code
-	*/
-	/// @endcond
-	
-	else if( strcmp( urlpath[ 1 ], "request" ) == 0 )
-	{
-		struct TagItem tags[] = {
-			{ HTTP_HEADER_CONTENT_TYPE, (FULONG)StringDuplicate( "text/html" ) },
-			{ HTTP_HEADER_CONNECTION, (FULONG)StringDuplicate( "close" ) },
-			{TAG_DONE, TAG_DONE}
-		};
-		
-		response = HttpNewSimple( HTTP_200_OK,  tags );
-		
-		char *params = NULL;
-		char *servername = NULL;
-		int type = 0;
-		char *path = NULL;
-		
-		DEBUG( "[NMWebRequest] request!!\n" );
-		
-		HashmapElement *el = NULL;
-		
-		el = HttpGetPOSTParameter( request, "params" );
-		if( el != NULL )
-		{
-			params = UrlDecodeToMem( (char *)el->data );
-			DEBUG( "[NMWebRequest] params %s!!\n", params );
-		}
-		
-		el = HttpGetPOSTParameter( request, "type" );
-		if( el != NULL )
-		{
-			type = atoi( (char *)el->data );
-			DEBUG( "[NMWebRequest] type %d!!\n", type );
-		}
-		
-		el = HttpGetPOSTParameter( request, "path" );
-		if( el != NULL )
-		{
-			path = UrlDecodeToMem( (char *)el->data );
-			DEBUG( "[NMWebRequest] group %s!!\n", path );
-		}
-		
-		el = HttpGetPOSTParameter( request, "servername" );
-		if( el != NULL )
-		{
-			servername = UrlDecodeToMem( (char *)el->data );
-			DEBUG( "[NMWebRequest] servername %s!!\n", servername );
-		}
-		
-		if( params != NULL && path != NULL )
-		{
-			char *serresp = NotificationManagerSendRequestToConnections( l->sl_NotificationManager, request, loggedSession, servername, type, path, params ); // 0 - type request, 1 - event
-			if( serresp != NULL )
-			{
-				HttpSetContent( response, serresp, strlen( serresp ) );
-			}
-		} // missing parameters
-		else
-		{
-			char buffer[ 256 ];
-			char buffer1[ 256 ];
-			snprintf( buffer1, sizeof(buffer1), l->sl_Dictionary->d_Msg[DICT_PARAMETERS_MISSING], "username, channelid, app, title, message" );
-			snprintf( buffer, sizeof(buffer), "fail<!--separate-->{ \"response\": \"%s\", \"code\":\"%d\" }", buffer1 , DICT_PARAMETERS_MISSING );
-			HttpAddTextContent( response, buffer );
-		}
-		
-		if( params != NULL )
-		{
-			FFree( params );
-		}
-
-		if( path != NULL )
-		{
-			FFree( path );
 		}
 	}
 
