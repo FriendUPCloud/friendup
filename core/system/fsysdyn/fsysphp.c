4195f2836f44d5f91b96f12cadacea8699b40380
stefkosdev <ps@friendup.cloud>
2020-03-30 10:17:40 +0200

    Timeout values can be overwritten in Config now (should solve problem with uploading big files)

--- core/system/fsysdyn/fsysphp.c
+++ core/system/fsysdyn/fsysphp.c
@@ -216,7 +216,7 @@ ListString *PHPCall( const char *command )
 	struct timeval timeout;
 
 	// Initialize the timeout data structure. 
-	timeout.tv_sec = MOD_TIMEOUT;
+	timeout.tv_sec = FILESYSTEM_MOD_TIMEOUT;
 	timeout.tv_usec = 0;
 
 	while( TRUE )
