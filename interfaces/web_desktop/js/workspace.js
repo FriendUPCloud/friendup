807ffda55eb1c9fc59804ebb7a179618e6cf3381
Hogne Titlestad <ht@friendup.cloud>
2020-04-02 17:10:02 +0200

    Another undefined fix.

--- interfaces/web_desktop/js/workspace.js
+++ interfaces/web_desktop/js/workspace.js
@@ -1120,19 +1120,22 @@ Workspace = {
 					}
 				}
 
+				var hasSessionID = null;
+				var hasLoginID = null;
+
 				try
 				{
 					Workspace.userLevel = json.level;
+					hasSessionID = ( typeof( json.sessionid ) != 'undefined' && json.sessionid && json.sessionid.length > 1 );
+					hasLoginID = ( typeof( json.loginid ) != 'undefined' && json.loginid && json.loginid.length > 1 );
 				}
 				catch( e )
 				{
 					Workspace.userLevel = false;
 				}
 
-				var hasSessionID = ( typeof( json.sessionid ) != 'undefined' && json.sessionid && json.sessionid.length > 1 );
-				var hasLoginID = ( typeof( json.loginid ) != 'undefined' && json.loginid && json.loginid.length > 1 );
 
-				if( json.result == '0' || hasSessionID || hasLoginID || json.result == 3 )
+				if( json && ( json.result == '0' || hasSessionID || hasLoginID || json.result == 3 ) )
 				{
 					// // console.log( 'Test2: We got a login.' );
 					
