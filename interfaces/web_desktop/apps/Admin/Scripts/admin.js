a58761e92dcacc0e3b2247f9c742b17af2c18ff8
Chris Andrè Strømland <chris@ideverket.no>
2020-04-21 11:55:47 +0200

    Version number from config next to name of app.

--- interfaces/web_desktop/apps/Admin/Scripts/admin.js
+++ interfaces/web_desktop/apps/Admin/Scripts/admin.js
@@ -10,42 +10,60 @@
 
 var AdminViews = {};
 
+var Config = { data: {}, raw: '' };
+
 Application.run = function( msg )
 {
-		
+	
 	this.sendMessage( { type: 'system', command: 'setsingleinstance', value: true } );
 	
-	var v = new View( {
-		title: 'Friend Admin',
-		width: 1280,
-		height: 960
-	} );
-	
-	this.mainView = v;
-	
-	var f = new File( 'Progdir:Templates/main.html' );
-	f.onLoad = function( data )
+	var d = new File( 'Progdir:Config.conf' );
+	d.onLoad = function( conf )
 	{
-		v.setContent( data );
-	}
-	f.load();
-	
-	v.onClose = function()
-	{
-		Application.quit();
-	}
-	
-	v.setMenuItems( [
+		
+		try
 		{
-			name: i18n( 'i18n_file' ),
-			items: [
-				{
-					name: i18n( 'i18n_about' ),
-					command: 'about'
-				}
-			]
+			Config.raw  = conf;
+			Config.data = JSON.parse( conf ); 
+			
+			console.log( Config.data );
 		}
-	] );
+		catch( e ){  }
+		
+		var v = new View( {
+			title: 'Friend Admin' + ( Config.data.Version ? ' (v' + Config.data.Version + ')' : '' ),
+			width: 1280,
+			height: 960
+		} );
+	
+		this.mainView = v;
+		
+		var f = new File( 'Progdir:Templates/main.html' );
+		f.onLoad = function( data )
+		{
+			v.setContent( data );
+		}
+		f.load();
+		
+		v.onClose = function()
+		{
+			Application.quit();
+		}
+		
+		v.setMenuItems( [
+			{
+				name: i18n( 'i18n_file' ),
+				items: [
+					{
+						name: i18n( 'i18n_about' ),
+						command: 'about'
+					}
+				]
+			}
+		] );
+		
+	}
+	d.load();
 	
 }
 
@@ -54,55 +72,37 @@ messageFunctions = {
 
 	about( msg )
 	{
-		console.log( msg );
+		//console.log( msg );
 		
 		if( !AdminViews[ 'about' ] )
 		{
 			
-			var d = new File( 'Progdir:Config.conf' );
-			d.onLoad = function( conf )
-			{
-				var obj = {
-					title: 'About Friend Admin',
-					width: 400,
-					height: 288
-				};
-				
-				var f = new File( 'Progdir:Templates/about.html' );
-				
-				try
-				{
-					var json = JSON.parse( conf ); 
-					
-					console.log( json );
-					
-					obj.title += ( ' (v' + json.Version + ')' );
-					
-					f.replacements = { content: /*JSON.stringify( */conf/*, null, 2 )*/ };
-				}
-				catch( e ){  }
-				
-				AdminViews[ 'about' ] = new View( obj );
-				
-				f.onLoad = function( data )
-				{
-					AdminViews[ 'about' ].setContent( data );
-				}
-				f.load();
-				
-				AdminViews[ 'about' ].onClose = function()
-				{
-					AdminViews[ 'about' ] = false;
-				}
-				
-			}
-			d.load();
+			AdminViews[ 'about' ] = new View( {
+				title: 'About Friend Admin' + ( Config.data.Version ? ' (v' + Config.data.Version + ')' : '' ),
+				width: 400,
+				height: 288
+			} );
 			
+			var f = new File( 'Progdir:Templates/about.html' );
+			f.replacements = { 
+				content: Config.raw 
+			};
+			f.onLoad = function( data )
+			{
+				AdminViews[ 'about' ].setContent( data );
+			}
+			f.load();
+			
+			AdminViews[ 'about' ].onClose = function()
+			{
+				AdminViews[ 'about' ] = false;
+			}
+				
 		}
 		
-		return Application.mainView.sendMessage( {
-			command: 'about'
-		} );
+		//return Application.mainView.sendMessage( {
+		//	command: 'about'
+		//} );
 	}
 	
 };
