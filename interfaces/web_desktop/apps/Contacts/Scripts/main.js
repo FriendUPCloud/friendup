5aefed99613cc12327b40f93b6a8fb65477003f5
Hogne Titlestad <ht@friendup.cloud>
2020-04-29 16:32:16 +0200

    Add contacts app.

---
+++ interfaces/web_desktop/apps/Contacts/Scripts/main.js
@@ -0,0 +1,33 @@
+/*©agpl*************************************************************************
+*                                                                              *
+* This file is part of FRIEND UNIFYING PLATFORM.                               *
+* Copyright (c) Friend Software Labs AS. All rights reserved.                  *
+*                                                                              *
+* Licensed under the Source EULA. Please refer to the copy of the GNU Affero   *
+* General Public License, found in the file license_agpl.txt.                  *
+*                                                                              *
+*****************************************************************************©*/
+
+var cview = null;
+
+Application.run = function( msg )
+{
+	cview = new View( {
+		title: i18n( 'i18n_contacts' ),
+		width: 600,
+		height: 600
+	} );
+	
+	cview.onClose = function()
+	{
+		Application.quit();
+	}
+	
+	let f = new File( 'Progdir:Templates/main.html' );
+	f.onLoad = function( data )
+	{
+		cview.setContent( data );
+	}
+	f.load();
+};
+
