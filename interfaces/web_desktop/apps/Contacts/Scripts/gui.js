92dbbdd61e9b8732347c4cc53fad224286b3b651
Hogne Titlestad <ht@friendup.cloud>
2020-04-29 17:36:03 +0200

    Added more GUI.

--- interfaces/web_desktop/apps/Contacts/Scripts/gui.js
+++ interfaces/web_desktop/apps/Contacts/Scripts/gui.js
@@ -8,27 +8,39 @@
 *                                                                              *
 *****************************************************************************©*/
 
-var cview = null;
-
 Application.run = function( msg )
 {
-	cview = new View( {
-		title: i18n( 'i18n_contacts' ),
-		width: 600,
-		height: 600
+	RefreshContacts();
+}
+
+function RefreshContacts( searchkeys )
+{
+	let m = new Module( 'system' );
+	m.onExecuted = function( e, d )
+	{
+		if( e != 'ok' )
+		{
+			ge( 'List' ).innerHTML = '<div class="List"><div class="HRow PaddingSmall sw1">' + i18n( 'i18n_no_contacts_available' ) + '</div></div>';
+			return;
+		}
+	};
+	m.execute( 'getcontacts' );
+}
+
+function AddContact()
+{
+	let cnt = new View( {
+		title: i18n( 'i18n_add_contact' ),
+		width: 400,
+		height: 400
 	} );
 	
-	cview.onClose = function()
+	let d = new File( 'Progdir:Templates/contact.html' );
+	d.i18n();
+	d.onLoad = function( data )
 	{
-		Application.quit();
+		cnt.setContent( data );
 	}
-	
-	let f = new File( 'Progdir:Templates/main.html' );
-	f.i18n();
-	f.onLoad = function( data )
-	{
-		cview.setContent( data );
-	}
-	f.load();
-};
+	d.load();
+}
 
