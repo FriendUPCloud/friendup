33089119ab3003d50a73fb6427361dc36130c1e7
Hogne Titlestad <ht@friendup.cloud>
2020-05-04 21:17:23 +0200

    Fix date.

--- modules/system/include/addcalendarevent.php
+++ modules/system/include/addcalendarevent.php
@@ -75,8 +75,8 @@ if( is_object( $args->args->event ) )
 	{
 		$timeto = date( 'Y-m-d H:i:s', strtotime( $o->Date . ' ' . $o->TimeTo ) );
 		$timefrom = date( 'Y-m-d H:i:s', strtotime( $o->Date . ' ' . $o->TimeFrom ) );
-		$utimeto = date( 'Ymd\THis\Z', strtotime( $o->TimeTo ) );
-		$utimefrom = date( 'Ymd\THis\Z', strtotime( $o->TimeFrom ) );
+		$utimeto = date( 'Ymd\THis', strtotime( $o->TimeTo ) );
+		$utimefrom = date( 'Ymd\THis', strtotime( $o->TimeFrom ) );
 		$timenow = date( 'Ymd\THis\Z' );
 	
 		// TODO: Implement template support!
