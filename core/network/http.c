349e4e7ccadf883dee15d154c0a99ca4434874f8
stefkosdev <ps@friendup.cloud>
2020-05-13 15:11:26 +0200

    WIP

--- core/network/http.c
+++ core/network/http.c
@@ -1028,9 +1028,9 @@ Content-Type: application/octet-stream
 					int divSize = strlen( http->http_PartDivider );
 
 					DEBUG("DIVSIZE %d\n", divSize );
-					FQUAD multipartLen = (http->http_SizeOfContent-(startOfFile-http->http_Content);
+					FQUAD multipartLen = (http->http_SizeOfContent-(startOfFile-http->http_Content) );
 					DEBUG("MULTIPART LEN %lu\n", multipartLen );
-					res = FindInBinaryPOS( http->http_PartDivider, divSize, startOfFile, multipartLen ) )-2;// + divSize;
+					res = FindInBinaryPOS( http->http_PartDivider, divSize, startOfFile, multipartLen )-2;// + divSize;
 					//res = FindInBinaryPOS( http->http_PartDivider, strlen(http->http_PartDivider), startOfFile, http->http_SizeOfContent ) - 2;
 					
 					//res = (FQUAD )FindInBinarySimple( http->http_PartDivider, divSize, startOfFile, http->http_SizeOfContent )-2;
