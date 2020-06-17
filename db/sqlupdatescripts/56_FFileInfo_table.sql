9106197ff1700a4d13e6876995aff88f29d45a09
Thomas Wollburg <tw@friendup.cloud>
2020-06-17 14:21:32 +0200

    New fileinfo table for replace .info files and reduce artifacts on filesystems

---
+++ db/sqlupdatescripts/56_FFileInfo_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `FFileInfo` (
+  `ID` bigint(20) NOT NULL,
+  `Name` varchar(255) NOT NULL DEFAULT '0',
+  `Path` varchar(1024) DEFAULT NULL,
+  `FilesystemID` bigint(20) NOT NULL DEFAULT '0',
+  `DateCreated` datetime NOT NULL,
+  `DateModified` datetime NOT NULL,
+  `Data` json NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
