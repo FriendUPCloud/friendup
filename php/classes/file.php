e2c9601274b23f934a2fdd33d46f113cdce8ff67
Hogne Titlestad <ht@friendup.cloud>
2020-06-26 09:29:17 +0200

    Remove big debug.

--- php/classes/file.php
+++ php/classes/file.php
@@ -195,7 +195,7 @@ class File
 		global $Config, $User, $Logger;
 		
 		$fd = new Door( reset( explode( ':', $this->path ) ) . ':', $this->_authcontext, $this->_authdata );
-		$Logger->log( '[File.class] ' . $this->_authcontext . ' -> ' . $this->_authdata . ' Checking file on filesystem: ' . print_r( $fd, 1 ) );
+		$Logger->log( '[File.class] ' . $this->_authcontext . ' -> ' . $this->_authdata );
 		$d = new dbIO( 'FFileInfo' );
 		$d->Path = $this->path;
 		$d->FilesystemID = $fd->ID;
