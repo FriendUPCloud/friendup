76c95efb0e66f079de71de0304f604b7d1c04b0a
Hogne Titlestad <ht@friendup.cloud>
2020-07-07 22:06:16 +0200

    Wrong variable for view.

--- interfaces/web_desktop/js/gui/directoryview.js
+++ interfaces/web_desktop/js/gui/directoryview.js
@@ -4455,7 +4455,7 @@ function OpenWindowByFileinfo( oFileInfo, event, iconObject, unique, targetView 
 								if( w.revent ) w.removeEvent( 'resize', w.revent );
 								w.revent = w.addEvent( 'resize', function( cbk )
 								{
-									win.directoryview.changed = true;
+									dv.directoryview.changed = true;
 									self.redrawIcons( false, self.direction, cbk );
 								} );
 								
