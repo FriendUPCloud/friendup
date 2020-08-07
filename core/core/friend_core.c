df69f99df81a8f2c769db6184901e7572e6b3e32
Hogne Titlestad <ht@friendup.cloud>
2020-08-07 09:09:28 +0200

    Increase stack.

--- core/core/friend_core.c
+++ core/core/friend_core.c
@@ -551,7 +551,7 @@ void *FriendCoreAcceptPhase2( void *data )
 					
 #ifdef USE_PTHREAD
 						//size_t stacksize = 16777216; //16 * 1024 * 1024;
-						size_t stacksize = 2097152;	// half of previous stack
+						size_t stacksize = 4194304;	// half of previous stack
 						pthread_attr_t attr;
 						pthread_attr_init( &attr );
 						pthread_attr_setstacksize( &attr, stacksize );
@@ -856,7 +856,7 @@ inline static void *FriendCoreAcceptPhase2( FriendCoreInstance *fc )
 					
 #ifdef USE_PTHREAD
 						//size_t stacksize = 16777216; //16 * 1024 * 1024;
-						size_t stacksize = 2097152;	// half of previous stack
+						size_t stacksize = 4194304;	// half of previous stack
 						pthread_attr_t attr;
 						pthread_attr_init( &attr );
 						pthread_attr_setstacksize( &attr, stacksize );
@@ -1932,7 +1932,7 @@ static inline void FriendCoreEpoll( FriendCoreInstance* fc )
 #ifdef USE_PTHREAD
 						//size_t stacksize = 16777216; //16 * 1024 * 1024;
 						//size_t stacksize = 8777216;	// half of previous stack
-						size_t stacksize = 524288; //512 * 1024;
+						size_t stacksize = 4194304; //512 * 1024;
 						pthread_attr_t attr;
 						pthread_attr_init( &attr );
 						pthread_attr_setstacksize( &attr, stacksize );
