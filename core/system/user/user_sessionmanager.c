232aea914e31d963b8f2e1b9602deda0ccaac049
Hogne Titlestad <ht@friendup.cloud>
2020-08-12 11:04:43 +0200

    Remove sessions properly with correct SQL query.

--- core/system/user/user_sessionmanager.c
+++ core/system/user/user_sessionmanager.c
@@ -876,7 +876,7 @@ int USMRemoveOldSessions( void *lsb )
 	DEBUG("USMRemoveOldSessions\n" );
 
 	BufString *sqlreq = BufStringNew();
-	BufStringAdd( sqlreq,  "DELETE from `FUserSession` WHERE SessionID in(" );
+	BufStringAdd( sqlreq,  "DELETE from `FUserSession` WHERE SessionID in(\"" );
 	char temp[ 512 ];
 	temp[ 0 ] = 0;
 	
@@ -939,7 +939,7 @@ int USMRemoveOldSessions( void *lsb )
 					remsessions[ nr++ ] = remSession;
 				}
 			}
-			BufStringAddSize( sqlreq, ")", 1 );
+			BufStringAddSize( sqlreq, "\")", 2 );
 		
 		    FRIEND_MUTEX_UNLOCK( &(smgr->usm_Mutex) );
 		}
