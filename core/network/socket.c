8b3f3b2caaea60d2d356efc9731dbaa60977aa02
Hogne Titlestad <ht@friendup.cloud>
2020-08-15 14:16:00 +0200

    Just normalize timeout.

--- core/network/socket.c
+++ core/network/socket.c
@@ -50,7 +50,7 @@ void SocketFree( Socket *sock );
 
 #define SOCKET_STATE_MAX_ACCEPTED_TIME_s 15 //socket has N seconds to send the first byte
 #define READ_TILL_END_BUFFER_SIZE 256000	//8192
-#define READ_TILL_END_SOCKET_TIMEOUT (15 * 1000)
+#define READ_TILL_END_SOCKET_TIMEOUT (10000)
 #define READ_PACKAGE_BUFFER_SIZE 128000
 
 static int ssl_session_ctx_id = 1;
