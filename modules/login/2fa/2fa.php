aaa60bc64a29fcc2cbb7cc1318679a2568440139
Chris Andrè Strømland <chris@ideverket.no>
2020-11-18 14:50:36 +0100

    Some progress, next steps, verify user and send sms code + login to FriendCore server side.

--- modules/login/2fa/2fa.php
+++ modules/login/2fa/2fa.php
@@ -1,348 +1,134 @@
 <?php
-	
+
 	error_reporting( E_ALL & ~E_NOTICE & ~E_DEPRECATED );
 	ini_set( 'display_errors', '1' );
 	
-	// TODO Simplify this on cleanup !!! ....
-	
-	if( isset( $GLOBALS['request_variables']['encrypted'] ) )
+	if( $args = getArgs() )
 	{
-		include_once( 'php/3rdparty/fcrypto/fcrypto.class.php' );
-		
-		if( file_exists( 'cfg/crt/key.pem' ) )
+		if( isset( $args->encrypted ) )
 		{
-			if( $privateKey = file_get_contents( 'cfg/crt/key.pem' ) )
+			include_once( 'php/3rdparty/fcrypto/fcrypto.class.php' );
+			
+			$fcrypt = new fcrypto(); $json = false;
+			
+			if( $keys = getServerKeys() )
 			{
-				$fcrypt = new fcrypto();
-				
-				$decrypted = $fcrypt->decryptString( $GLOBALS['request_variables']['encrypted'], $privateKey );
-				
-				die( $GLOBALS['request_variables']['encrypted'] . ' [] ' . print_r( $decrypted,1 ) . ' [] ' . $privateKey );
-				
-				if( $decrypted && $decrypted->plaintext )
+				if( $keys->privatekey )
 				{
-					if( $json = json_decode( $decrypted->plaintext ) )
+					/*$decrypted = $fcrypt->decryptString( $args->encrypted, $keys->privatekey );
+					
+					if( $decrypted && $decrypted->plaintext )
 					{
-						// Setup mysql abstraction
-						if( file_exists( 'cfg/cfg.ini' ) )
-						{
-							$configfilesettings = parse_ini_file( 'cfg/cfg.ini', true );
-							include_once( 'php/classes/dbio.php' );
-							
-							// Set config object
-							$Config = new stdClass();
-							$car = array( 'Hostname', 'Username', 'Password', 'DbName',
-										  'FCHost', 'FCPort', 'FCUpload', 
-										  'SSLEnable', 'FCOnLocalhost', 'Domains' );
-							
-							foreach( array(
-								'host', 'login', 'password', 'dbname', 
-								'fchost', 'fcport', 'fcupload',
-								'SSLEnable', 'fconlocalhost', 'domains'
-							) as $k=>$type )
-							{
-								$val = '';
-								switch( $type )
-								{
-									case 'host':
-									case 'login':
-									case 'password':
-									case 'dbname':
-										$val = isset( $configfilesettings['DatabaseUser'][$type] ) ? $configfilesettings['DatabaseUser'][$type] : '';
-										break;	
-									
-									case 'fchost':
-									case 'fcport':
-									case 'fcupload':
-									case 'fconlocalhost':
-										$val = isset( $configfilesettings['FriendCore'][$type] ) ? $configfilesettings['FriendCore'][$type] : '';
-										break;
-										
-									case 'SSLEnable':	
-										$val = isset( $configfilesettings['Core'][$type] ) ? $configfilesettings['Core'][$type] : '';
-										break;
-										
-									case 'domains':
-										$val = isset( $configfilesettings['Security'][$type] ) ? $configfilesettings['Security'][$type] : '';
-										break;		
-									default:
-										$val = '';
-										break;	
-								}
-								$Config->{$car[$k]} = $val;
-							}
-							
-							$SqlDatabase = new SqlDatabase();
-							if( !$SqlDatabase->Open( $Config->Hostname, $Config->Username, $Config->Password ) )
-							{
-								die( 'fail<!--separate-->Could not connect to database.' );
-							}
-							$SqlDatabase->SelectDatabase( $Config->DbName );
-							
-							// DeviceID, PublicKey and Username
-							
-							if( $usr = $SqlDatabase->FetchObject( '
-								SELECT * 
-								FROM FUser 
-								WHERE Name = \'' . $json->username . '\' 
-							' ) )
-							{
-								// If PublicKey is missing add it once and use the PublicKey from the database to send data back to client
-								
-								$json->publickey = $fcrypt->encodeKeyHeader( trim( $json->publickey ) );
-								
-								if( !isset( $usr->PublicKey ) || !$usr->PublicKey )
-								{
-									$d = new DbTable( 'FUser', $SqlDatabase );
-									if( $d->LoadTable() )
-									{
-										$publickey = false;
-										
-										if ( isset( $d->_fieldnames ) )
-										{
-											foreach( $d->_fieldnames as $f )
-											{
-												if( $f == 'PublicKey' )
-												{
-													$publickey = true;
-												}
-											}
-											
-											if( !$publickey )
-											{
-												$d->AddField ( 'PublicKey', 'text', array ( 'after'=>'Password' ) );
-											}
-										}
-									}
-									
-									$u = new dbIO( 'FUser', $SqlDatabase );
-									$u->ID = $usr->ID;
-									if( $u->Load() )
-									{
-										if( isset( $u->PublicKey ) )
-										{
-											$u->PublicKey = $json->publickey;
-											$u->Save();
-										
-											$usr->PublicKey = $u->PublicKey;
-										}
-									}
-								}
-								
-								if( !$usr->PublicKey || ( $json->publickey != $usr->PublicKey ) )
-								{
-									//die( print_r( $json,1 ) . ' [] ' . print_r( $usr,1 ) );
-									die( 'failed ... wrong login information' );
-								}
-								
-								$s = new dbIO( 'FUserSession', $SqlDatabase );
-								$s->UserID = $usr->ID;
-								$s->DeviceIdentity = $json->deviceid;
-								if( !$s->Load() )
-								{
-									$s->SessionID = hash( 'sha256', ( time().$usr->Name.rand(0,999).rand(0,999).rand(0,999) ) );
-								}
-								$s->LoggedTime = time();
-								$s->Save();
-								
-								if( $s->ID > 0 && ( $ses = $SqlDatabase->FetchObject( '
-									SELECT * 
-									FROM FUserSession 
-									WHERE ID = \'' . $s->ID . '\' 
-									AND UserID = \'' . $usr->ID . '\' 
-								' ) ) )
-								{
-									$ret = new stdClass();
-									$ret->result    = 0;
-									$ret->userid    = $usr->ID;
-									$ret->fullname  = $usr->FullName;
-									$ret->sessionid = $ses->SessionID;
-									$ret->loginid   = $ses->ID;
-									
-									//die( json_encode( $ret ) . ' [] ' . $usr->PublicKey );
-									
-									$encrypted = $fcrypt->encryptString( json_encode( $ret ), $usr->PublicKey );
-									
-									if( $encrypted && $encrypted->cipher )
-									{
-										die( $encrypted->cipher );
-									}
-								}
-							}
-						}
+						$json = json_decode( $decrypted->plaintext );
+					}*/
+					
+					if( $decrypted = $fcrypt->decryptRSA( $args->encrypted, $keys->privatekey ) )
+					{
+						$json = json_decode( $decrypted );
 					}
 				}
 			}
-		}
-		
-		die( 'fail ......' );
-	}
-	else if( isset( $GLOBALS['request_variables']['data'] ) )
-	{
-		//error_reporting( E_ALL );
-		//ini_set( 'display_errors', '1' );
-		
-		include_once( 'php/3rdparty/fcrypto/fcrypto.class.php' );
-		
-		if( $json = json_decode( $GLOBALS['request_variables']['data'] ) )
-		{
-			// Setup mysql abstraction
-			if( file_exists( 'cfg/cfg.ini' ) )
+			
+			
+			
+			if( $args->publickey )
 			{
-				$configfilesettings = parse_ini_file( 'cfg/cfg.ini', true );
-				include_once( 'php/classes/dbio.php' );
+				// TODO: Find out why this didn't work ???
+				//$encrypted = $fcrypt->encryptRSA( json_encode( $json ), $args->publickey );
+				//die( $encrypted );
 				
-				// Set config object
-				$Config = new stdClass();
-				$car = array( 'Hostname', 'Username', 'Password', 'DbName',
-							  'FCHost', 'FCPort', 'FCUpload', 
-							  'SSLEnable', 'FCOnLocalhost', 'Domains' );
-				
-				foreach( array(
-					'host', 'login', 'password', 'dbname', 
-					'fchost', 'fcport', 'fcupload',
-					'SSLEnable', 'fconlocalhost', 'domains'
-				) as $k=>$type )
+				$encrypted = $fcrypt->encryptString( json_encode( $json ), $args->publickey );
+				die( $encrypted->cipher );
+			}
+			
+			// TODO: Do some checking in the database for 2 factor mobile number on the user wanting to login, then send sms confirmation code and render form on client side.
+			
+			// TODO: Setup receiving sms confirmation code and login to FriendCore once SMS code is confirmed.
+			
+			// check user data before login into FriendCore and asking for sessionid to return to client (encrypted)
+			
+			if( $login = remoteAuth( '/system.library/login', 
+				array(
+					'username'  => $json->username, 
+					'password'  => $json->password, 
+					'deviceid'  => $json->deviceid 
+			) ) )
+			{
+				if( $ses = json_decode( $login ) )
 				{
-					$val = '';
-					switch( $type )
+					if( $ses->sessionid )
 					{
-						case 'host':
-						case 'login':
-						case 'password':
-						case 'dbname':
-							$val = isset( $configfilesettings['DatabaseUser'][$type] ) ? $configfilesettings['DatabaseUser'][$type] : '';
-							break;	
-						
-						case 'fchost':
-						case 'fcport':
-						case 'fcupload':
-						case 'fconlocalhost':
-							$val = isset( $configfilesettings['FriendCore'][$type] ) ? $configfilesettings['FriendCore'][$type] : '';
-							break;
-							
-						case 'SSLEnable':	
-							$val = isset( $configfilesettings['Core'][$type] ) ? $configfilesettings['Core'][$type] : '';
-							break;
-							
-						case 'domains':
-							$val = isset( $configfilesettings['Security'][$type] ) ? $configfilesettings['Security'][$type] : '';
-							break;		
-						default:
-							$val = '';
-							break;	
-					}
-					$Config->{$car[$k]} = $val;
-				}
-				
-				$SqlDatabase = new SqlDatabase();
-				if( !$SqlDatabase->Open( $Config->Hostname, $Config->Username, $Config->Password ) )
-				{
-					die( 'fail<!--separate-->Could not connect to database.' );
-				}
-				$SqlDatabase->SelectDatabase( $Config->DbName );
-				
-				// DeviceID, PublicKey and Username
-						
-				if( $usr = $SqlDatabase->FetchObject( '
-					SELECT * 
-					FROM FUser 
-					WHERE Name = \'' . $json->username . '\' 
-				' ) )
-				{
-					// If PublicKey is missing add it once and use the PublicKey from the database to send data back to client
-					
-					$fcrypt = new fcrypto();
-					
-					$json->publickey = $fcrypt->encodeKeyHeader( trim( $json->publickey ) );
-					
-					if( !isset( $usr->PublicKey ) || !$usr->PublicKey )
-					{
-						$d = new dbTable( 'FUser', $SqlDatabase );
-						if( $d->LoadTable() )
-						{
-							$publickey = false;
-							
-							if ( isset( $d->_fieldnames ) )
-							{
-								foreach( $d->_fieldnames as $f )
-								{
-									if( $f == 'PublicKey' )
-									{
-										$publickey = true;
-									}
-								}
-								
-								if( !$publickey )
-								{
-									$d->AddField ( 'PublicKey', 'text', array ( 'after'=>'Password' ) );
-								}
-							}
-						}
-						
-						$u = new dbIO( 'FUser', $SqlDatabase );
-						$u->ID = $usr->ID;
-						if( $u->Load() )
-						{
-							if( isset( $u->PublicKey ) )
-							{
-								$u->PublicKey = $json->publickey;
-								$u->Save();
-							
-								$usr->PublicKey = $u->PublicKey;
-							}
-						}
-					}
-					
-					if( !$usr->PublicKey || ( $json->publickey != $usr->PublicKey ) )
-					{
-						//die( print_r( $json,1 ) . ' [] ' . print_r( $usr,1 ) . ' [] "' . $json->publickey . '" [] "' . $usr->PublicKey . '"' );
-						die( 'failed ... wrong login information' );
-					}
-					
-					$s = new dbIO( 'FUserSession', $SqlDatabase );
-					$s->UserID = $usr->ID;
-					$s->DeviceIdentity = $json->deviceid;
-					if( !$s->Load() )
-					{
-						$s->SessionID = hash( 'sha256', ( time().$usr->Name.rand(0,999).rand(0,999).rand(0,999) ) );
-					}
-					$s->LoggedTime = time();
-					$s->Save();
-					
-					if( $s->ID > 0 && ( $ses = $SqlDatabase->FetchObject( '
-						SELECT * 
-						FROM FUserSession 
-						WHERE ID = \'' . $s->ID . '\' 
-						AND UserID = \'' . $usr->ID . '\' 
-					' ) ) )
-					{
-						$ret = new stdClass();
-						$ret->result    = 0;
-						$ret->userid    = $usr->ID;
-						$ret->fullname  = $usr->FullName;
-						$ret->sessionid = $ses->SessionID;
-						$ret->loginid   = $ses->ID;
-						
-						$encrypted = $fcrypt->encryptString( json_encode( $ret ), $usr->PublicKey );
-						
-						//die( json_encode( $ret ) . ' [] ' . $usr->PublicKey . ' [] ' . $json->publickey );
-						
-						if( $encrypted && $encrypted->cipher )
-						{
-							die( $encrypted->cipher );
-						}
+						die( json_encode( $ses ) );
 					}
 				}
 			}
+			
+			die( 'fail ... ' . ( $login ? $login : '' ) );
+			
 		}
-		
-		die( 'fail ......' );
 	}
+
 	
 	/*
 		define some functions.
 	*/
+	
+	function getArgs()
+	{
+		$args = new stdClass();
+		
+		if( isset( $GLOBALS['request_path'] ) && $GLOBALS['request_path'] && strstr( $GLOBALS['request_path'], '/loginprompt/' ) )
+		{
+			if( $url = explode( '/loginprompt/', $GLOBALS['request_path'] ) )
+			{
+				if( isset( $url[1] ) && $url[1] )
+				{
+					$args->publickey = $url[1];
+				}
+			}
+		}
+		
+		if( isset( $GLOBALS['request_variables'] ) && $GLOBALS['request_variables'] )
+		{
+			foreach( $GLOBALS['request_variables'] as $k => $v )
+			{
+				if( $k && $k != '(null)' )
+				{
+					$args->{$k} = $v;
+				}
+			}
+		}
+		
+		return $args;
+	}
+	
+	function getServerKeys()
+	{
+		$pem = new stdClass();
+		$pem->privatekey = null;
+		$pem->publickey = null;
+		
+		if( file_exists( 'cfg/crt/server_encryption_key.pem' ) )
+		{
+			if( $keys = file_get_contents( 'cfg/crt/server_encryption_key.pem' ) )
+			{
+				if( strstr( $keys, '-----' . "\r\n" . '-----' ) && ( $keys = explode( '-----' . "\r\n" . '-----', $keys ) ) )
+				{
+					if( isset( $keys[0] ) )
+					{
+						$pem->privatekey = ( $keys[0] . '-----' );
+					}
+					if( isset( $keys[1] ) )
+					{
+						$pem->publickey = ( '-----' . $keys[1] );
+					}
+				}
+			}
+		}
+		
+		return $pem;
+	}
+	
 	function renderSecureLoginForm()
 	{
 		$providers = [];
@@ -366,16 +152,19 @@
 	
 	function renderReplacements( $template )
 	{
-		$welcome = $GLOBALS['login_modules']['saml']['Login']['logintitle_en'] !== null ? $GLOBALS['login_modules']['saml']['Login']['logintitle_en'] :'Secure Login';
+		$welcome = $GLOBALS['login_modules']['saml']['Login']['logintitle_en'] !== null ? $GLOBALS['login_modules']['saml']['Login']['logintitle_en'] :'2FA Login';
 		$samlendpoint = $GLOBALS['login_modules']['saml']['Module']['samlendpoint'] !== null ? $GLOBALS['login_modules']['saml']['Module']['samlendpoint'] : 'about:blank';
 		
 		$samlendpoint .= '?friendendpoint=' . urlencode($GLOBALS['request_path']);
 		
 		$publickey = '';
 		
-		if( file_exists( 'cfg/crt/key.pub' ) )
+		if( $keys = getServerKeys() )
 		{
-			$publickey = file_get_contents( 'cfg/crt/key.pub' );
+			if( $keys->publickey )
+			{
+				$publickey = base64_encode( $keys->publickey );
+			}
 		}
 		
 		$finds = [
@@ -393,7 +182,139 @@
 		
 		return str_replace($finds, $replacements, $template);
 	}
-
+	
+	
+	
+	function remoteAuth( $url, $args = false, $method = 'POST', $headers = false, $auth = false )
+	{
+		$configpath = __DIR__ . '/../../../cfg/cfg.ini';
+		
+		$conf = parse_ini_file( $configpath, true );
+		
+		$url = ( $conf['Core']['SSLEnable'] ? 'https://' : 'http://' ) . $conf['FriendCore']['fchost'] . ( $conf['FriendCore']['port'] ? ':' . $conf['FriendCore']['port'] : '' ) . $url;
+		
+		//die( $url . ' -- ' );
+		
+		// TODO: Check why curl won't get localhost ???, maybe SSL needs to be off or on ??? Look at some old code using localhost with curl ...
+		
+		$curl = curl_init();
+		
+		if( $headers && $auth && $auth['username'] && $auth['password'] )
+		{
+			$base64 = base64_encode( trim( $auth['username'] ) . ':' . trim( $auth['password'] ) );
+			
+			$headers[] = ( 'Authorization: Basic ' . $base64 );
+		}
+		
+		if( $url && strstr( $url, '?' ) )
+		{
+			$thispath = $url;
+		
+			$url = explode( '?', $url );
+		
+			if( isset( $url[1] ) )
+			{
+				if( strstr( $url[1], '&' ) && strstr( $url[1], '=' ) )
+				{
+					$url[1] = explode( '&', $url[1] );
+				
+					foreach( $url[1] as $k=>$p )
+					{
+						if( strstr( $url[1][$k], '=' ) )
+						{
+							$url[1][$k] = explode( '=', $url[1][$k] );
+						
+							if( isset( $url[1][$k][1] ) )
+							{
+								$url[1][$k][1] = urlencode( $url[1][$k][1] );
+							}
+						
+							$url[1][$k] = implode( '=', $url[1][$k] );
+						}
+					}
+				
+					$url[1] = implode( '&', $url[1] );
+				}
+				else if( strstr( $url[1], '=' ) )
+				{
+					$url[1] = explode( '=', $url[1] );
+				
+					if( isset( $url[1][1] ) )
+					{
+						$url[1][1] = urlencode( $url[1][1] );
+					}
+				
+					$url[1] = implode( '=', $url[1] );
+				}
+			}
+		
+			$url = implode( '?', $url );
+		}
+	
+		curl_setopt( $curl, CURLOPT_URL, $url );
+	
+		if( $headers )
+		{
+			curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers );
+		}
+	
+		if( $method != 'POST' )
+		{
+			curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, $method );
+		}
+	
+		if( $args )
+		{
+			if( is_object( $args ) )
+			{
+				$args = array(
+					'args' => urlencode( json_encode( $args ) )
+				);
+			}
+			else if( is_string( $args ) )
+			{
+				$args = array(
+					'args' => urlencode( $args )
+				);
+			}
+			
+			$json = false;
+			
+			if( $headers )
+			{
+				foreach( $headers as $v )
+				{
+					if( strstr( $v, 'application/json' ) )
+					{
+						$json = true;
+					}
+				}
+			
+				if( $json )
+				{
+					$args = json_encode( $args );
+				}
+			}
+			
+			curl_setopt( $curl, CURLOPT_POST, true );
+			curl_setopt( $curl, CURLOPT_POSTFIELDS, $args );
+		}
+	
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+	
+		$output = curl_exec( $curl );
+	
+		$httpCode = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
+	
+		//die( $url . ' [] ' . $output . ' || ' . print_r( $httpCode,1 ) );
+	
+		curl_close( $curl );
+		
+		return $output;
+	}
+	
+	
+	
 	//render the form
 	renderSecureLoginForm();
 
