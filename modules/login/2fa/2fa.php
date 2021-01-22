6ff783f20355e4972ce79ec52ed53349703c8019
Hogne Titlestad <ht@friendup.cloud>
2021-01-22 08:30:58 +0100

    Add license.

--- modules/login/2fa/2fa.php
+++ modules/login/2fa/2fa.php
@@ -1,5 +1,15 @@
 <?php
 
+/*©lgpl*************************************************************************
+*                                                                              *
+* This file is part of FRIEND UNIFYING PLATFORM.                               *
+* Copyright (c) Friend Software Labs AS. All rights reserved.                  *
+*                                                                              *
+* Licensed under the Source EULA. Please refer to the copy of the GNU Lesser   *
+* General Public License, found in the file license_lgpl.txt.                  *
+*                                                                              *
+*****************************************************************************©*/
+
 	error_reporting( E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_WARNING );
 	ini_set( 'display_errors', '1' );
 	
