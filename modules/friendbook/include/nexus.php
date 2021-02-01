ac7bdce093cc4915ddf2807953d1da5638f055c5
Hogne Titlestad <ht@friendup.cloud>
2021-02-01 20:24:34 +0100

    Add basic FriendBook skeleton.

---
+++ modules/friendbook/include/nexus.php
@@ -0,0 +1,22 @@
+<?php
+
+/*©lgpl*************************************************************************
+*                                                                              *
+* This file is part of FRIEND UNIFYING PLATFORM.                               *
+* Copyright (c) Friend Software Labs AS. All rights reserved.                  *
+*                                                                              *
+* Licensed under the Source EULA. Please refer to the copy of the GNU Lesser   *
+* General Public License, found in the file license_lgpl.txt.                  *
+*                                                                              *
+*****************************************************************************©*/
+
+class Nexus
+{
+    // Check the host for information
+    public function checkhost( $vars, $args )
+    {
+    
+    }
+}
+
+?>
