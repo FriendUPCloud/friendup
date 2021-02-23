0360df372c5acaa58813b117b6997897478930e2
Hogne Titlestad <ht@friendup.cloud>
2021-02-23 14:08:23 +0100

    Make savestate more sane.

--- modules/system/include/savestate.php
+++ modules/system/include/savestate.php
@@ -9,12 +9,16 @@
 *                                                                              *
 *****************************************************************************©*/
 
-$SqlDatabase->Query( '
-	UPDATE FUserApplication SET `Data`=\'' . mysqli_real_escape_string( $SqlDatabase->_link, $args->args->state ) . '\'
-	WHERE
-		UserID=\'' . $User->ID . '\' AND
-		ID in ( SELECT UserApplicationID FROM `FAppSession` WHERE AuthID=\'' . mysqli_real_escape_string( $SqlDatabase->_link, $args->args->authId ) .'\')
-' );
-die( 'ok' );
+if( isset( $args->args->authId ) )
+{
+	$SqlDatabase->Query( '
+		UPDATE FUserApplication SET `Data`=\'' . mysqli_real_escape_string( $SqlDatabase->_link, $args->args->state ) . '\'
+		WHERE
+			UserID=\'' . $User->ID . '\' AND
+			ID in ( SELECT UserApplicationID FROM `FAppSession` WHERE AuthID=\'' . mysqli_real_escape_string( $SqlDatabase->_link, $args->args->authId ) .'\')
+	' );
+	die( 'ok' );
+}
+die( 'fail<!--separate-->{"response":"-1","message":"No valid authid provided."}' );
 
 ?>
