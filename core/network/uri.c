b40f8cf2dd96e19f2c4407b62b23319e3df12cfb
Hogne Titlestad <ht@friendup.cloud>
2021-02-24 16:06:17 +0100

    Remove debug

--- core/network/uri.c
+++ core/network/uri.c
@@ -619,7 +619,7 @@ Uri* UriParse( char* str )
 
 	// Get query --------------------------------------------------------------
 	char* query = UriGetQuery( next, remainingLen, &next );
-	DEBUG( "Testing UriGetQuery: %s\n", query );
+	//DEBUG( "Testing UriGetQuery: %s\n", query );
 	remainingLen = strLen - ( next - str );
 	if( query != NULL )
 	{
