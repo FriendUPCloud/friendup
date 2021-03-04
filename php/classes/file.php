bc6e9e1b0ae206ab13c864dfb3db40cb8dbcbd57
Hogne Titlestad <ht@friendup.cloud>
2021-03-04 14:07:25 +0100

    Remove logging.

--- php/classes/file.php
+++ php/classes/file.php
@@ -25,8 +25,6 @@ class File
 
 	function File( $path )
 	{
-		global $Logger;
-		$Logger->log( "Before decode: " . $path );
 		$this->path = urldecode( $path );
 		
 		$this->GetAuthContextComponent();
