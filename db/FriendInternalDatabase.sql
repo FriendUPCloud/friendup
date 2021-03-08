19ca38604a4d63101332f1b56952cd5fc7befefc
Hogne Titlestad <ht@friendup.cloud>
2021-03-08 12:38:13 +0100

    Update Script

--- db/FriendInternalDatabase.sql
+++ db/FriendInternalDatabase.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS `FDBUpdate` (
   `Filename` varchar(255) NOT NULL,
   `Created` bigint(20) NOT NULL,
   `Updated` bigint(20) NOT NULL,
+  `Script` varchar(1024) NOT NULL DEFAULT '',
   `Error` varchar(1024) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
