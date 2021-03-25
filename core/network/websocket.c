ed990ef542d04bb2e636d986fb63f88d3d6fda45
Hogne Titlestad <ht@friendup.cloud>
2021-03-25 07:53:47 +0100

    Change timeout.

--- core/network/websocket.c
+++ core/network/websocket.c
@@ -333,8 +333,8 @@ WebSocket *WebSocketNew( void *sb,  int port, FBOOL sslOn, int proto, FBOOL extD
 		ws->ws_Info.ssl_cert_filepath = ws->ws_CertPath;
 		ws->ws_Info.ssl_private_key_filepath = ws->ws_KeyPath;
 		ws->ws_Info.options = ws->ws_Opts;// | LWS_SERVER_OPTION_REQUIRE_VALID_OPENSSL_CLIENT_CERT;
-		ws->ws_Info.timeout_secs = 8;
-		ws->ws_Info.timeout_secs_ah_idle = 8;
+		ws->ws_Info.timeout_secs = 20;
+		ws->ws_Info.timeout_secs_ah_idle = 20;
 		if( ws->ws_UseSSL == TRUE ) 
 		{
 			ws->ws_Info.options |= LWS_SERVER_OPTION_REDIRECT_HTTP_TO_HTTPS;
