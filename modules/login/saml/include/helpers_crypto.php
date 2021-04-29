31d73d07049dbec1761cf38b94a7818367966716
Hogne Titlestad <ht@friendup.cloud>
2021-04-29 14:39:27 +0200

    Add crypto functions.

---
+++ modules/login/saml/include/helpers_crypto.php
@@ -0,0 +1,31 @@
+<?php
+
+
+function getServerKeys()
+{
+	$pem = new stdClass();
+	$pem->privatekey = null;
+	$pem->publickey = null;
+	
+	if( file_exists( __DIR__ . '../../cfg/crt/server_encryption_key.pem' ) )
+	{
+		if( $keys = file_get_contents( __DIR__ . '../../cfg/crt/server_encryption_key.pem' ) )
+		{
+			if( strstr( $keys, '-----' . "\r\n" . '-----' ) && ( $keys = explode( '-----' . "\r\n" . '-----', $keys ) ) )
+			{
+				if( isset( $keys[0] ) )
+				{
+					$pem->privatekey = ( $keys[0] . '-----' );
+				}
+				if( isset( $keys[1] ) )
+				{
+					$pem->publickey = ( '-----' . $keys[1] );
+				}
+			}
+		}
+	}
+	
+	return $pem;
+}
+
+?>
