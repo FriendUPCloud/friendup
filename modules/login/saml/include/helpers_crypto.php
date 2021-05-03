79dfde02d1d0867ebe8d379b9366bf433d01848b
Hogne Titlestad <ht@friendup.cloud>
2021-05-03 10:25:54 +0200

    Add license header.

--- modules/login/saml/include/helpers_crypto.php
+++ modules/login/saml/include/helpers_crypto.php
@@ -1,5 +1,16 @@
 <?php
 
+/*©lgpl*************************************************************************
+*                                                                              *
+* Friend Unifying Platform                                                     *
+* ------------------------                                                     *
+*                                                                              *
+* Copyright 2014-2017 Friend Software Labs AS, all rights reserved.            *
+* Hillevaagsveien 14, 4016 Stavanger, Norway                                   *
+* Tel.: (+47) 40 72 96 56                                                      *
+* Mail: info@friendos.com                                                      *
+*                                                                              *
+*****************************************************************************©*/
 
 function getServerKeys()
 {
