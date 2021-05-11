a2791697ef6e54fe0f9f786b0da1304936336765
Chris Andrè Strømland <chris@ideverket.no>
2021-05-11 16:43:46 +0200

    Flow between client and server, TODO: Create / Validate user then login.

--- modules/login/google/google.php
+++ modules/login/google/google.php
@@ -20,12 +20,13 @@ ini_set( 'display_errors', '1' );
 // Get command line arguments
 if( $args = getArgs() )
 {
+	
+	$json = false;
+	
 	// Check if the request is encrypted(?)
 	if( isset( $args->encrypted ) )
 	{
 		$json = receive_encrypted_json( $args->encrypted );
-		
-		$mode = ( $server && $server->server ? $server->server : 'default' );
 		
 		if( $args->publickey )
 		{
@@ -33,6 +34,65 @@ if( $args = getArgs() )
 			$json->publickey = $args->publickey;
 		}
 		
+		if( $args->deviceid )
+		{
+			// Client deviceid ...
+			$json->deviceid = $args->deviceid;
+		}
+	}
+	else if( isset( $args->token ) )
+	{
+		$json = $args;
+	}
+	
+	if( $json )
+	{
+		//POST /token HTTP/1.1
+		//Host: oauth2.googleapis.com
+		//Content-Type: application/x-www-form-urlencoded
+
+		//code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7&
+		//client_id=your-client-id&
+		//client_secret=your-client-secret&
+		//redirect_uri=https%3A//oauth2.example.com/code&
+		//grant_type=authorization_code
+		
+		//$obj = new stdClass();
+		//$obj->code          = $json->code;
+		//$obj->client_id     = '1053224064374-tc9k39g33m49ljrhrhu3rfp68mpfpsdd.apps.googleusercontent.com';
+		//$obj->client_secret = 'or2FUaZPWGZT-0xGj7W_Bp7X';
+		//$obj->grant_type    = 'authorization_code';
+		
+		$curl = curl_init();
+		curl_setopt( $curl, CURLOPT_URL, 'https://oauth2.googleapis.com/tokeninfo?id_token=' . $json->token );
+		
+		//curl_setopt( $curl, CURLOPT_HTTPHEADER, [ 'Authorization: Bearer ' . $json->access_token/*'Content-type: application/json'*/ ] );
+		curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, 'GET' );
+		
+		//curl_setopt( $curl, CURLOPT_POST, true );
+		//curl_setopt( $curl, CURLOPT_POSTFIELDS, json_encode( $obj ) );
+		
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+	
+		$output = curl_exec( $curl );
+		
+		curl_close( $curl );
+		
+		if( $output )
+		{
+			if( $output = json_decode( $output ) )
+			{
+				if( $output->sub && $output->kid )
+				{
+					$json->username = $output->sub;
+					$json->password = ( 'HASHED' . hash( 'sha256', $output->kid ) );
+				}
+			}
+		}
+		
+		die( print_r( $json, 1 ) . ' -- ' . print_r( $output, 1 ) );
+		
+		// TODO: Auth with google to get name, email, avatar, etc
 		
 		// Check authentication mode
 		switch( $mode )
