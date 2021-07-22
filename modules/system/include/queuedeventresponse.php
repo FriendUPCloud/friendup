c9a7345656c9e9edbd30b612ea30c4e4f50a0a8a
Hogne Titlestad <ht@friendup.cloud>
2021-07-22 15:22:03 +0200

    Add response actions.

--- modules/system/include/queuedeventresponse.php
+++ modules/system/include/queuedeventresponse.php
@@ -13,7 +13,40 @@
 $o = new dbIO( 'FQueuedEvent' );
 if( $o->Load( $args->args->eventId ) )
 {
+	$response = new stdClass();
 	
+	if( $args->args->response === false )
+	{
+		$o->Status = 'rejected';
+		if( isset( $o->ActionRejected ) )
+		{
+			if( $action = json_decode( $o->ActionRejected ) )
+			{
+				if( file_exists( 'modules/' . $action->module . '.php' ) )
+				{
+					$args = $action->args;
+					include( 'modules/' . $action->module . '.php' );
+				}
+			}
+		}
+	}
+	else if( $args->args->response === true )
+	{
+		$o->Status = 'accepted';
+		if( isset( $o->ActionAccepted ) )
+		{
+			if( $action = json_decode( $o->ActionAccepted ) )
+			{
+				if( file_exists( 'modules/' . $action->module . '.php' ) )
+				{
+					$args = $action->args;
+					include( 'modules/' . $action->module . '.php' );
+				}
+			}
+		}
+	}
+	$o->Save();
+	die( 'ok<!--separate-->' . $response );
 }
 
 die( 'fail<!--separate-->{"response":-1,"message":"Failed to find queued event."}' );
