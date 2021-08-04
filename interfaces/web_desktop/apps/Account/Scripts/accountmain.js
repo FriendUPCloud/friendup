8c706d620ff9513d0593fc1c5642612238a1c633
Hogne Titlestad <ht@friendup.cloud>
2021-08-04 13:01:23 +0200

    Labels

--- interfaces/web_desktop/apps/Account/Scripts/accountmain.js
+++ interfaces/web_desktop/apps/Account/Scripts/accountmain.js
@@ -170,6 +170,11 @@ function refreshGroups( keys )
 		let str = '<hr class="Divider"/>';
 		
 		str += '<div class="Padding"><h2>' + i18n( 'i18n_other_groups' ) + '</h2><div class="List">';
+		str += '<div class="HRow">\
+				<div class="PaddingSmall FloatLeft HContent40"><strong>' + i18n( 'i18n_group_name' ) + ':</strong></div>\
+				<div class="PaddingSmall FloatLeft HContent40"><strong>' + i18n( 'i18n_owner' ) + ':</strong></div>\
+				<div class="PaddingSmall FloatLeft HContent20 TextRight"></div>\
+			</div>';
 		let sw = 2;
 		for( let a = 0; a < d.length; a++ )
 		{
