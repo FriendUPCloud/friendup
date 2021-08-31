94899ccb86ef6d25dde48d7b399f4d8daee07146
Espen Olsen <eo@friendup.cloud>
2021-08-31 10:47:25 +0000

    asd

--- db/sqlupdatemanuallyinternal/1_refresh_token.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `FRefreshToken` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Token` varchar(512) NOT NULL,
-  `DeviceID` varchar(512) NOT NULL,
-  `UserID` bigint(20) NOT NULL,
-  `CreatedTime` bigint(20) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
