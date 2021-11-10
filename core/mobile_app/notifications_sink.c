20571996ef9504d80d0e94e7b17e2d5599a42570
stefkosdev <ps@friendup.cloud>
2021-11-10 10:12:00 +0100

    FIX - requestid field in response

--- core/mobile_app/notifications_sink.c
+++ core/mobile_app/notifications_sink.c
@@ -400,7 +400,7 @@ void NotifyUsers( NotificationMessage *nm )
 		FFree( nm->userNameList );
 	
 		char reply[256];
-		int msize = sprintf(reply + LWS_PRE, "{\"type\":\"service\",\"data\":{\"type\":\"notification\",\"requestid\":%s\"data\":{\"status\":%d}}}", nm->requestId, returnStatus );
+		int msize = sprintf(reply + LWS_PRE, "{\"type\":\"service\",\"data\":{\"type\":\"notification\",\"requestid\":\"%s\",\"data\":{\"status\":%d}}}", nm->requestId, returnStatus );
 #ifdef WEBSOCKET_SEND_QUEUE
 		WriteMessageSink( nm->d, (unsigned char *)reply+LWS_PRE, msize );
 #else
