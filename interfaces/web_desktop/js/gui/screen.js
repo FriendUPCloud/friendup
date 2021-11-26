92ec5e0c556699a106d232b01225ef5aab89330c
Hogne Titlestad <ht@friendup.cloud>
2021-11-26 15:01:35 +0100

    Check static screen height.

--- interfaces/web_desktop/js/gui/screen.js
+++ interfaces/web_desktop/js/gui/screen.js
@@ -42,7 +42,7 @@ Screen = function ( flags, initObject )
 		{
 			if( this.resized || this.resized == 'uninitialized' )
 			{
-				this.contentDivHeight = parseInt( getComputedStyle( this.contentDiv ).height );
+				this.contentDivHeight = window.innerHeight - this.div.screenTitle.offsetHeight;
 				this.resized = false;
 			}
 		    console.log( 'Current content div height: ' + this.contentDivHeight );
