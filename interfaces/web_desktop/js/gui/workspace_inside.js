42733062f92c638ce4787c847d667026ade00ccb
Hogne Titlestad <ht@friendup.cloud>
2021-12-03 15:58:01 +0100

    Wait for theme to load.

--- interfaces/web_desktop/js/gui/workspace_inside.js
+++ interfaces/web_desktop/js/gui/workspace_inside.js
@@ -3835,7 +3835,7 @@ body .View.Active.IconWindow ::-webkit-scrollbar-thumb
 					styles.type = 'text/css';
 					styles.onload = function()
 					{
-						console.log( 'THE CSS HAS LOADED!!' );
+						document.body.classList.add( 'ThemeLoaded' );
 						// We are inside (wait for wallpaper) - watchdog
 						if( !Workspace.insideInterval )
 						{
