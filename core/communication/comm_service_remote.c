7dad7f01d1bcf6bfb0bd43e1f5740051a0c6121e
stefkosdev <ps@friendup.cloud>
2021-12-10 14:52:43 +0100

    WIP

--- core/communication/comm_service_remote.c
+++ core/communication/comm_service_remote.c
@@ -728,7 +728,7 @@ void *RemoteSocketProcessSockBlock( void *fcv )
 		int tr = 5;
 		char locBuffer[ bufferSize ];
 		
-		SocketSetBlocking( th->sock, TRUE );
+		//SocketSetBlocking( th->sock, TRUE );
 		//bs = SocketReadPackage( sock );
 		//bs = LocalSocketRead( th->sock, 0, 15 );
 		//bs = th->sock->s_Interface->SocketReadTillEnd( th->sock, 0, 15 );
