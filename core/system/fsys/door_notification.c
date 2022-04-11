04910f0ce0333a9c1bf68c301daf775d8d169c6d
stefkosdev <ps@friendup.cloud>
2022-04-11 19:47:10 +0200

    FIX - delete old doornotification

--- core/system/fsys/door_notification.c
+++ core/system/fsys/door_notification.c
@@ -426,10 +426,10 @@ int DoorNotificationRemoveEntries( void *lsb )
 	{
 		DEBUG("[DoorNotificationRemoveEntries] start\n");
 		char temp[ 1024 ];
-		time_t acttime = time( NULL );
+		time_t acttime = time( NULL ) - 86400;
 		
 		// we remove old entries older then 24 hours
-		snprintf( temp, sizeof(temp), "DELETE from `FDoorNotification` WHERE (%lu-Time)>86400", acttime );
+		snprintf( temp, sizeof(temp), "DELETE from `FDoorNotification` WHERE 'Time' < %lu", acttime );
 		
 		sqllib->QueryWithoutResults( sqllib, temp );
 		
