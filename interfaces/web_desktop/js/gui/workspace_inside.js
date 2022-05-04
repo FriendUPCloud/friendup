ffc5f9ba3bdf40bb153e75320d6337b14741d784
Hogne Titlestad <ht@friendup.cloud>
2022-05-04 12:04:31 +0200

    Debug

--- interfaces/web_desktop/js/gui/workspace_inside.js
+++ interfaces/web_desktop/js/gui/workspace_inside.js
@@ -8223,6 +8223,7 @@ body .View.Active.IconWindow ::-webkit-scrollbar-thumb
 		
 		path = encodeURIComponent( path );
 		path.split( '#' ).join( '%23' );
+		console.log( 'PATH: ' + path );
 		let dowloadURI = document.location.protocol +'//'+ document.location.host +'/system.library/file/read/' + fn + '?mode=rs&sessionid=' + Workspace.sessionId + '&path='+ path + '&download=1';
 		
 		//check if we are inside one of our apps with a custom download handler....
