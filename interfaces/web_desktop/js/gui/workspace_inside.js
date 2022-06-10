fafc772cec560f67827e57e777b3f3ca4863e36b
Hogne Titlestad <ht@friendup.cloud>
2022-06-10 16:04:37 +0200

    Remove loop

--- interfaces/web_desktop/js/gui/workspace_inside.js
+++ interfaces/web_desktop/js/gui/workspace_inside.js
@@ -2380,7 +2380,7 @@ body .View.Active.IconWindow ::-webkit-scrollbar-thumb
 			function initFriendWorkspace()
 			{
 				// Make sure we have loaded
-				if( Workspace.mode != 'vr' && ( Workspace.screen && Workspace.screen.contentDiv ) )
+				if( !Workspace.dashboard && Workspace.mode != 'vr' && ( Workspace.screen && Workspace.screen.contentDiv ) )
 				{
 					if( Workspace.screen.contentDiv.offsetHeight < 100 )
 					{
