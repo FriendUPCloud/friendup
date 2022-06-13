2869acccdb5701cbdb83eb9bcd33faaa1e07daee
stefkosdev <ps@friendup.cloud>
2022-06-13 13:14:00 +0200

    Memory leak fix

--- libs/mysql/mysqllibrary.c
+++ libs/mysql/mysqllibrary.c
@@ -225,6 +225,8 @@ void *Load( struct SQLLibrary *l, FULONG *descr, char *where, int *entries )
 									memcpy( tmpval, row[i], lengths[i] );
 									// Add tmpval to string pointer list..
 									memcpy( strptr + dptr[2], &tmpval, sizeof( char * ) );
+									
+									FFree( tmpval );
 								}
 							}
 							else
