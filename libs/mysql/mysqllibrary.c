238c7bae7e7e3344f5d730eded24a834e895a1ee
stefkosdev <ps@friendup.cloud>
2022-06-29 12:05:31 +0200

    DEBUG added

--- libs/mysql/mysqllibrary.c
+++ libs/mysql/mysqllibrary.c
@@ -185,6 +185,7 @@ void *Load( struct SQLLibrary *l, FULONG *descr, char *where, int *entries )
 		// While the column is not the last
 		while( dptr[0] != SQLT_END )
 		{
+			printf( " id %d\n", dptr[0] );
 			switch( dptr[ 0 ] )
 			{
 				case SQLT_NODE:
@@ -192,7 +193,7 @@ void *Load( struct SQLLibrary *l, FULONG *descr, char *where, int *entries )
 						dataUsed = 1;
 						MinNode *locnode = (MinNode *)strptr + dptr[ 2 ];
 						
-						DEBUG("Node %p locnode %d\n", node, locnode );
+						printf("Node %p locnode %p\n", node, locnode );
 						
 						if( node != NULL )
 						{
