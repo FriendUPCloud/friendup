f2934374f2553336ae9c546b787f347b43f5b13b
Hogne Titlestad <ht@friendos.com>
2022-08-02 15:07:55 +0200

    Add blocking

--- core/system/fsysdyn/fsysphp.c
+++ core/system/fsysdyn/fsysphp.c
@@ -230,7 +230,7 @@ ListString *PHPCall( const char *command )
 	fds[1].events = POLLOUT;
 	
 	// Set to non block
-	fcntl( fds[1].fd, F_SETFL, O_NONBLOCK );
+	fcntl( fds[1].fd, F_SETFL, O_BLOCK );
 	
 	int ret = 0;
 
