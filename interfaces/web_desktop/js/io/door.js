4f7b2ad6bbe21a4c029d50c1b2300c22be8aa242
Hogne Titlestad <ht@friendos.com>
2022-08-02 11:51:00 +0200

    Support empty folders

--- interfaces/web_desktop/js/io/door.js
+++ interfaces/web_desktop/js/io/door.js
@@ -297,7 +297,7 @@ Door.prototype.getIcons = function( fileInfo, callback, flags )
 					}
 					
 					console.log( 'CPY - What is list: ' + d );
-					let list = d.indexOf( '{' ) && parsed ? parsed : {};
+					let list = d.indexOf( '[' ) >= 0 && parsed ? parsed : {};
 					
 					if( typeof( list ) == 'object' && list.length )
 					{
