f88c253e59594a30aad952ef1b82444bf7a1f11f
Hogne Titlestad <ht@friendup.cloud>
2022-08-09 10:13:16 +0200

    Test usleep

--- core/core/friend_core.c
+++ core/core/friend_core.c
@@ -1890,6 +1890,7 @@ static inline void FriendCoreEpoll( FriendCoreInstance* fc )
 	// All incoming network events go through here
 	while( !fc->fci_Shutdown )
 	{
+		usleep( 0 );
 #ifdef SINGLE_SHOT
 		epoll_ctl( fc->fci_Epollfd, EPOLL_CTL_MOD, fc->fci_Sockets->fd, &(fc->fci_EpollEvent) );
 #endif
@@ -2130,7 +2131,6 @@ static inline void FriendCoreEpoll( FriendCoreInstance* fc )
 			
 			}
 		}
-		usleep( 0 );
 	}
 	
 	//DEBUG("End main loop\n");
