8594611bac5beb802e98d08aa7be7f55c1d47a96
Hogne Titlestad <ht@friendup.cloud>
2022-08-09 12:52:02 +0200

    Fix

--- core/system/user/user_session.c
+++ core/system/user/user_session.c
@@ -350,7 +350,7 @@ int UserSessionWebsocketWrite( UserSession *us, unsigned char *msgptr, int msgle
 						wsd->wsc_InUseCounter++;
 						FRIEND_MUTEX_UNLOCK( &(wsd->wsc_Mutex) );
 					
-						if( wsd->wsc_Wsi != NULL && wsd->wsc_Status != WSC_STATUS_DELETED ) && wsd->wsc_Status != WSC_STATUS_TO_BE_REMOVED )
+						if( wsd->wsc_Wsi != NULL && wsd->wsc_Status != WSC_STATUS_DELETED && wsd->wsc_Status != WSC_STATUS_TO_BE_REMOVED )
 						{
 							lws_callback_on_writable( ((WSCData *)us->us_WSD)->wsc_Wsi );
 						}
