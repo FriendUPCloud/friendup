92c7f9fb30e249ce447a022a2e61d5cbbbd125d7
Hogne Titlestad <ht@friendup.cloud>
2022-08-10 13:20:54 +0200

    Debug

--- interfaces/web_desktop/js/gui/directoryview.js
+++ interfaces/web_desktop/js/gui/directoryview.js
@@ -4193,7 +4193,7 @@ function OpenWindowByFileinfo( oFileInfo, event, iconObject, unique, targetView,
 	let fromFolder = false;
 	if( currentMovable && currentMovable.content && currentMovable.content.directoryview )
 		fromFolder = true;
-	
+	console.log( fromFolder ? 'From folder' : 'Not from folder' );
 	// Make a copy of fileinfo
 	let fileInfo = {};
 	for( let a in oFileInfo )
