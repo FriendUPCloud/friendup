0911f2978f082c6e1a23f1996dd0e8a2c8c1beaa
Hogne Titlestad <ht@friendup.cloud>
2022-08-15 13:34:00 +0200

    Undo

--- core/system/fsys/fs_manager_web.c
+++ core/system/fsys/fs_manager_web.c
@@ -1943,8 +1943,7 @@ Http *FSMWebRequest( void *m, char **urlpath, Http *request, UserSession *logged
 											
 												File *wfp = (File *)dsthand->FileOpen( dstrootf, dstpath, "w+" );
 											
-//#define COPY_BUFFER_SIZE 524288
-#define COPY_BUFFER_SIZE 16384
+#define COPY_BUFFER_SIZE 524288
 											
 												if( wfp != NULL )
 												{
@@ -1989,7 +1988,6 @@ Http *FSMWebRequest( void *m, char **urlpath, Http *request, UserSession *logged
 																	break;
 																}
 															}
-															usleep( 25 );
 														}
 														FFree( dataBuffer );
 														
