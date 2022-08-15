61ffe7679c5c07600b7c5fcd59fb3b7ff4eb1459
Hogne Titlestad <ht@friendup.cloud>
2022-08-15 13:49:23 +0200

    Move stuff to http

--- interfaces/web_desktop/js/io/cajax.js
+++ interfaces/web_desktop/js/io/cajax.js
@@ -425,6 +425,8 @@ cAjax.prototype.open = function( method, url, syncing, hasReturnCode )
 		url.indexOf( 'http' ) != 0 && 
 		url.indexOf( 'system.library' ) >= 0 &&
 		url.indexOf( '/file/read' ) < 0 &&
+		url.indexOf( '/file/copy' ) < 0 &&
+		url.indexOf( '/file/delete' ) < 0 &&
 		url.indexOf( '/file/write' ) < 0
 	)
 	{	
