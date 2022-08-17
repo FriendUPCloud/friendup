1ec7faded7b6fba7edc525ea2f996458de9d1570
Hogne Titlestad <ht@friendup.cloud>
2022-08-17 12:18:41 +0200

    Fix

--- interfaces/web_desktop/js/gui/window.js
+++ interfaces/web_desktop/js/gui/window.js
@@ -1944,7 +1944,7 @@ function CloseView( win, delayed )
 		if( !win.parentNode.parentNode ) return;
 		if( win.parentNode.classList.contains( 'ViewContainer' ) )
 		{
-			win.parentNode.parentNode.classList.add( 'Closing', 'NoEvents' );
+			win.parentNode.classList.add( 'Closing', 'NoEvents' );
 		}
 		
 		if( win.parentNode.classList.contains( 'Dialog' ) || 
