29bfd4254ffd41a74f59510afda4a28c5b20e7f0
Hogne Titlestad <ht@friendup.cloud>
2022-11-25 14:17:10 +0100

    Correct path

--- interfaces/web_desktop/js/gui/directoryview.js
+++ interfaces/web_desktop/js/gui/directoryview.js
@@ -4514,7 +4514,7 @@ function OpenWindowByFileinfo( oFileInfo, event, iconObject, unique, targetView,
         f.onLoad = function( data )
         {
             console.log( 'What goes around', data );
-            pdfjsLib.GlobalWorkerOptions.workerSrc = '/web_desktop/3rdparty/pdfjs/build/pdf.worker.js';
+            pdfjsLib.GlobalWorkerOptions.workerSrc = '/webclient/3rdparty/pdfjs/build/pdf.worker.js';
             //'//mozilla.github.io/pdf.js/build/pdf.worker.js';
             // Using DocumentInitParameters object to load binary data.
             let loadingTask = pdfjsLib.getDocument( { data: data } );
