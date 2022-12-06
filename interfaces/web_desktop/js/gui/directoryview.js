b98a01461e2e64e208017f39c59bae3f165ee2fd
Hogne Titlestad <ht@friendup.cloud>
2022-12-06 14:27:35 +0100

    Test.

--- interfaces/web_desktop/js/gui/directoryview.js
+++ interfaces/web_desktop/js/gui/directoryview.js
@@ -4490,7 +4490,6 @@ function OpenWindowByFileinfo( oFileInfo, event, iconObject, unique, targetView,
 		{
 		    iconObject.applicationId = fileInfo.applicationId;
 		}
-		currentMovable = true; // Temporary state
 		Friend.startImageViewer( iconObject, { parentView: currentMovable, recent: fromFolder ? false : 'dashboard' } );
 	}
 	else if( iconObject.extension.toLowerCase() == 'pdf' )
