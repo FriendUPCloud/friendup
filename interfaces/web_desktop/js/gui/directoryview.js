77807dd17192e2994542434b71bf909f42119d76
Hogne Titlestad <ht@friendup.cloud>
2022-12-06 14:24:30 +0100

    Set temporary state

--- interfaces/web_desktop/js/gui/directoryview.js
+++ interfaces/web_desktop/js/gui/directoryview.js
@@ -4490,7 +4490,7 @@ function OpenWindowByFileinfo( oFileInfo, event, iconObject, unique, targetView,
 		{
 		    iconObject.applicationId = fileInfo.applicationId;
 		}
-		console.log( "starting viewer" );
+		currentMovable = true; // Temporary state
 		Friend.startImageViewer( iconObject, { parentView: currentMovable, recent: fromFolder ? false : 'dashboard' } );
 	}
 	else if( iconObject.extension.toLowerCase() == 'pdf' )
