62d43ea7fc64562ed4a97fe68841951d5b393c42
autocaret <autocaret@proton.me>
2023-03-06 09:35:07 +0100

    Fix

--- interfaces/web_desktop/js/gui/directoryview.js
+++ interfaces/web_desktop/js/gui/directoryview.js
@@ -4135,7 +4135,7 @@ FileIcon.prototype.Init = function( fileInfo, flags )
 		        {
 		            if( eventName == 'ondblclick' )
 		            {
-		                if( !( this.directoryView.filedialog && isMobile && this.directoryView.doubleclickfiles ) )
+		                if( this.directoryView.filedialog && this.directoryView.doubleclickfiles )
                 	    {
                 	        return;
                 	    }
