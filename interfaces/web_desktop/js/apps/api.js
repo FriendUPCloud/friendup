8d113db39c5cb808428c33455954b00e58b0966a
autocaret <autocaret@proton.me>
2023-03-14 13:42:08 +0100

    Test again

--- interfaces/web_desktop/js/apps/api.js
+++ interfaces/web_desktop/js/apps/api.js
@@ -5751,7 +5751,7 @@ function setupMessageFunction( dataPacket, origin )
 	    if( msg.type == 'view' )
 	    {
 	        if( msg.data )
-    	        console.log( 'What is the message: ', JSON.parse( JSON.stringify( msg.data ) ) );
+    	        msg.data = JSON.parse( JSON.stringify( msg.data ) );
         }
 		// Convert some data formats in a JSON complient structure
 		for( let a in msg )
