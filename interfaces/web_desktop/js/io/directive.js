6d1d4593d5958949364ed3e65826b5c97dd837f0
Hogne Titlestad <ht@friendup.cloud>
2023-04-04 14:31:52 +0200

    More debug

--- interfaces/web_desktop/js/io/directive.js
+++ interfaces/web_desktop/js/io/directive.js
@@ -124,21 +124,28 @@ function ExecuteApplication( app, args, callback, retries, flags )
 	// You need to wait with opening apps until they are loaded by app name
 	if( _executionQueue[ appName ] )
 	{
+		// Send message to already running app
+		if( args )
+		{
+			if( Friend.singleInstanceApps && Friend.singleInstanceApps[ appName ] )
+			{
+				let nmsg = {
+					command: 'cliarguments',
+					args: args
+				};
+				Friend.singleInstanceApps[ appName ].contentWindow.postMessage( JSON.stringify( nmsg ), '*' );
+				console.log( 'Tried to post message directly to running single instance app: ', appName, args );
+				if( callback )
+					callback( false, { response: false, message: 'Already run.', data: 'executed' } );
+				return;
+			}
+		}
 		console.log( 'ExecuteApplication - app found in execution queue', {
 			app   : app,
 			queue : _executionQueue,
 		});
 		if( callback )
 			callback( false, { response: false, message: 'Already run.', data: 'executed' } );
-		// Send message to already running app
-		if( args )
-		{
-			if( Friend.singleInstanceApps && Friend.singleInstanceApps[ appName ] )
-			{
-				Friend.singleInstanceApps[ appName ].sendMessage( args );
-				console.log( 'Tried to post message directly to running app: ', appName, args );
-			}
-		}
 		return;
 	}
 
