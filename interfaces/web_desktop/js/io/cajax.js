dca6ea4d4da8592cdfc27e4cb66bde20f0948ce4
Espen Olsen <eo@friendup.cloud>
2023-05-02 09:45:51 +0000

    test

--- interfaces/web_desktop/js/io/cajax.js
+++ interfaces/web_desktop/js/io/cajax.js
@@ -435,11 +435,12 @@ cAjax.prototype.open = function( method, url, syncing, hasReturnCode )
 	// Move dos calls onto http
 	let dosCall = false;
 	if( 
-		url.indexOf( '/file/read' ) >= 0 ||
-		url.indexOf( '/file/copy' ) >= 0 ||
+		url.indexOf( '/file/read'   ) >= 0 ||
+		url.indexOf( '/file/copy'   ) >= 0 ||
 		url.indexOf( '/file/delete' ) >= 0 ||
-		url.indexOf( '/file/write' ) >= 0 ||
-		url.indexOf( '/file/dir' )
+		url.indexOf( '/file/write'  ) >= 0 ||
+		url.indexOf( '/file/dir'    ) >= 0 ||
+		url.indexOf( '/file/expose' )
 	)
 	{
 		dosCall = true;
@@ -500,10 +501,10 @@ cAjax.prototype.open = function( method, url, syncing, hasReturnCode )
 	else
 	{
 		this.lastOptions = {
-			method: method,
-			url: url,
-			syncing: syncing,
-			hasReturnCode: hasReturnCode
+			method        : method,
+			url           : url,
+			syncing       : syncing,
+			hasReturnCode : hasReturnCode
 		};
 	
 		if( !method ) method = this.method ? this.method : 'POST';
