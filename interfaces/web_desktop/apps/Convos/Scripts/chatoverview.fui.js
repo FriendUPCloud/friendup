f92f02b06acc64eddee058ad5fa72629bd873a83
Hogne Titlestad <ht@friendup.cloud>
2023-07-03 17:41:22 +0200

    Add chat overview class stub.

---
+++ interfaces/web_desktop/apps/Convos/Scripts/chatoverview.fui.js
@@ -0,0 +1,50 @@
+class FUIChatoverview extends FUIElement
+{
+    constructor( options )
+    {
+        super( options );
+        // Do stuff
+    }
+    attachDomElement()
+    {
+        super.attachDomElement();
+        
+        let self = this;
+        
+        this.domElement.className = 'FUIChatoverview';
+        
+        // Set stuff on this.domElement.innerHTML
+    }
+    grabAttributes( domElement )
+    {
+        super.grabAttributes( domElement );
+        
+        let uniqueId = domElement.getAttribute( 'uniqueid' );
+        if( uniqueId ) this.options.uniqueid = uniqueId;
+    }
+    refreshDom( evaluated = false )
+    {
+        super.refreshDom();
+        let self = this;
+    }
+    // Get markup for object
+    getMarkup( data )
+    {
+    	// Return meta-markup for class instantiation later
+    	let props = '';
+    	let n = 0;
+    	for( let a in this.options )
+    	{
+    	    if( n++ > 0 ) props += ' ';
+    	    props += a + '="' + props[ a ] + '"';
+    	}
+    	return '<fui-billinglist' + props + '></fui-billinglist>';
+    }
+    // Just display an error message
+    errorMessage( string )
+    {
+        this.domElement.innerHTML = '<h2 class="Error">' + string + '</h2>';
+    }
+}
+FUI.registerClass( 'chatoverview', FUIChatoverview );
+
