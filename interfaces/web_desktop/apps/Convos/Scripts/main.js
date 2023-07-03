97164f7757ba64340cc7e66adf0d2b255ca991c0
Hogne Titlestad <ht@friendup.cloud>
2023-07-03 11:30:28 +0200

    Add important files to Convos.

--- interfaces/web_desktop/apps/Convos/Scripts/main.js
+++ interfaces/web_desktop/apps/Convos/Scripts/main.js
@@ -1,4 +1,8 @@
 Application.run = function( msg )
 {
+	let v = new View( {
+		title: 'Convos',
+		assets: [ 'Progdir:Scripts/vue.global.js', 'Progdir:Scripts/convos.js', 'Progdir:Markup/main.html' ],
+	} );
 }
 
