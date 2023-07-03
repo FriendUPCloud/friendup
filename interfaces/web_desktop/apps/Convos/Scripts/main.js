c2be5f16ac42ba2e5f4249ca2940e97d5fafd07a
Hogne Titlestad <ht@friendup.cloud>
2023-07-03 11:42:13 +0200

    Check convos.

--- interfaces/web_desktop/apps/Convos/Scripts/main.js
+++ interfaces/web_desktop/apps/Convos/Scripts/main.js
@@ -3,6 +3,8 @@ Application.run = function( msg )
 	let v = new View( {
 		title: 'Convos',
 		assets: [ 'Progdir:Scripts/vue.global.js', 'Progdir:Scripts/convos.js', 'Progdir:Markup/main.html' ],
+		width: 600,
+		height: 900,
 	} );
 }
 
