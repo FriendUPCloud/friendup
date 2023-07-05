4d3f6d933f1fcdb57ea8143be9ef57e6bad18ea1
autocaret <autocaret@proton.me>
2023-07-05 12:52:22 +0200

    Add contacts element.

---
+++ interfaces/web_desktop/apps/Convos/Scripts/contacts.fui.js
@@ -0,0 +1,57 @@
+class FUIContacts extends FUIElement
+{
+    constructor( options )
+    {
+        super( options );
+        // Do stuff
+    }
+    attachDomElement()
+    {
+        super.attachDomElement();
+        
+        let self = this;
+        
+        this.domElement.className = 'FUIContacts';
+        
+        let data = '\
+        <div class="Contacts"></div>
+        ';
+        
+        // Set stuff on this.domElement.innerHTML
+        this.refreshDom();
+    }
+    grabAttributes( domElement )
+    {
+        super.grabAttributes( domElement );
+        
+        let uniqueId = domElement.getAttribute( 'uniqueid' );
+        if( uniqueId ) this.options.uniqueid = uniqueId;
+    }
+    refreshDom( evaluated = false )
+    {
+        super.refreshDom();
+        let self = this;
+        
+        console.log( 'Refreshing dom!' );
+    }
+    // Get markup for object
+    getMarkup( data )
+    {
+    	// Return meta-markup for class instantiation later
+    	let props = '';
+    	let n = 0;
+    	for( let a in this.options )
+    	{
+    	    if( n++ > 0 ) props += ' ';
+    	    props += a + '="' + props[ a ] + '"';
+    	}
+    	return '<fui-contacts' + props + '></fui-contacts>';
+    }
+    // Just display an error message
+    errorMessage( string )
+    {
+        this.domElement.innerHTML = '<h2 class="Error">' + string + '</h2>';
+    }
+}
+FUI.registerClass( 'contacts', FUIContacts );
+
