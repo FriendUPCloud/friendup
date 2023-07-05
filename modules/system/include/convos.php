a46877d0ca17dad203b8b965d9296b14856a7daa
autocaret <autocaret@proton.me>
2023-07-05 12:55:22 +0200

    Add licensing.

--- modules/system/include/convos.php
+++ modules/system/include/convos.php
@@ -1,5 +1,15 @@
 <?php
 
+/*©mit**************************************************************************
+*                                                                              *
+* This file is part of FRIEND UNIFYING PLATFORM.                               *
+* Copyright (c) Friend Software Labs AS. All rights reserved.                  *
+*                                                                              *
+* Licensed under the Source EULA. Please refer to the copy of the MIT License, *
+* found in the file license_mit.txt.                                           *
+*                                                                              *
+*****************************************************************************©*/
+
 $response = new stdClass();
 
 if( isset( $args->args ) )
