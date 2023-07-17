dbaafa3db95dd7ae9aa93734c53c3a198863389d
Hogne Titlestad <ht@friendup.cloud>
2023-07-17 13:07:14 +0200

    Fix memset.

--- core/system/user/user_manager_web.c
+++ core/system/user/user_manager_web.c
@@ -570,7 +570,7 @@ Http *UMWebRequest( void *m, char **urlpath, Http *request, UserSession *loggedS
 					if( 1 )
 					{
 						int lenmsg = 0;	
-						memset( tmpmsg, "\0", msgsize );
+						memset( tmpmsg, 0, msgsize );
 						
 						if( appname != NULL )
 						{
