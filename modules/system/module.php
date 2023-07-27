be2f230db0526d11cba074533c089222d21265d3
Hogne Titlestad <ht@friendup.cloud>
2023-07-27 08:43:49 +0200

    Fix.

--- modules/system/module.php
+++ modules/system/module.php
@@ -305,7 +305,7 @@ if( isset( $args->command ) )
 			if( $s->Load() )
 			{
 				$keys = json_decode( $s->Data );
-				$key = preg_replace( '/--.*?--\\n/i', '', $keys->publicKey );
+				$key = preg_replace( '/--.*?--\\s/i', '', $keys->publicKey );
 				die( 'ok<!--separate-->' . trim( $key ) );
 			}
 			die( 'fail<!--separate-->{"message":"Could not load VAPID key.","response":-1} ');
