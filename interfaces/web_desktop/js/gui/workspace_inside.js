870cfe585703e5a626fb71001a926931216cfe6d
Hogne Titlestad <ht@friendup.cloud>
2023-07-28 10:35:34 +0200

    Add silence to mobile.

--- interfaces/web_desktop/js/gui/workspace_inside.js
+++ interfaces/web_desktop/js/gui/workspace_inside.js
@@ -3,6 +3,12 @@ Friend.startupApps = {};
 
 window.Sounds = {};
 Sounds.newMessage = new Audio('/themes/friendup13/sound/new_message.ogg');
+Sounds.silence = new Audio('/themes/friendup13/sound/silence.ogg');
+if( isMobile )
+{
+	Sounds.silence.loop = true;
+	Sounds.silence.play();
+}
 
 // Added to workspace
 let WorkspaceInside = {
