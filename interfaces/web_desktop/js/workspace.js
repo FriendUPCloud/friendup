623323c39dbcf8a31cd513e0b11f3cb724b711b1
Hogne Titlestad <ht@friendup.cloud>
2023-08-02 14:26:08 +0200

    Fix

--- interfaces/web_desktop/js/workspace.js
+++ interfaces/web_desktop/js/workspace.js
@@ -137,6 +137,7 @@ Workspace = {
 											// TODO: Remove double encoding issue
 											// Fix string
 											const rawData = window.atob( base64String );
+											return rawData;
 											//const padding = '='.repeat( ( 4 - base64String.length % 4 ) % 4 );
 											//const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
 											//const rawData = window.atob( base64 );
