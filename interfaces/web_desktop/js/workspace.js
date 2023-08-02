a5f2183d6d7b88cfe23d9089f441b56026aa198b
Hogne Titlestad <ht@friendup.cloud>
2023-08-02 14:12:20 +0200

    Fi?

--- interfaces/web_desktop/js/workspace.js
+++ interfaces/web_desktop/js/workspace.js
@@ -134,8 +134,10 @@ Workspace = {
 									.then( serviceWorkerRegistration => {
 										function urlBase64ToUint8Array( base64String )
 										{
-											const rawData = window.atob( base64String );
-											console.log( 'Here we go: ', rawData );
+											// Fix string
+											const padding = '='.repeat( ( 4 - base64String.length % 4 ) % 4 );
+											const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+											const rawData = window.atob( base64 );
 											
 											const outputArray = new Uint8Array( rawData.length );
 											for( let i = 0; i < rawData.length; ++i )
