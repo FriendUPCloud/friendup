796260e96b18c7a7e4d8a0c6af75c09bd020b9ff
Hogne Titlestad <ht@friendup.cloud>
2023-08-02 14:14:06 +0200

    Test

--- interfaces/web_desktop/js/workspace.js
+++ interfaces/web_desktop/js/workspace.js
@@ -139,6 +139,8 @@ Workspace = {
 											const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
 											const rawData = window.atob( base64 );
 											
+											console.log( 'It became: ', rawData, 'or', base64 );
+											
 											const outputArray = new Uint8Array( rawData.length );
 											for( let i = 0; i < rawData.length; ++i )
 											{
