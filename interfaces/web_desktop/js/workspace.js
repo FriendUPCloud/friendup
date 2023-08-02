f76813c39b1e0b19072557903860c3dee3128b70
Hogne Titlestad <ht@friendup.cloud>
2023-08-02 14:25:01 +0200

    Fix again

--- interfaces/web_desktop/js/workspace.js
+++ interfaces/web_desktop/js/workspace.js
@@ -136,10 +136,10 @@ Workspace = {
 										{
 											// TODO: Remove double encoding issue
 											// Fix string
-											base64string = window.atob( base64String );
-											const padding = '='.repeat( ( 4 - base64String.length % 4 ) % 4 );
-											const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-											const rawData = window.atob( base64 );
+											const rawData = window.atob( base64String );
+											//const padding = '='.repeat( ( 4 - base64String.length % 4 ) % 4 );
+											//const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+											//const rawData = window.atob( base64 );
 											
 											console.log( 'Here it is in url safe: ' + rawData );
 											const outputArray = new Uint8Array( rawData.length );
