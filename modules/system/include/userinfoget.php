56d239b3b7b26907e40e559ccb1ad9ffc5d22147
Hogne Titlestad <ht@friendup.cloud>
2023-08-03 15:12:29 +0200

    More fix

--- modules/system/include/userinfoget.php
+++ modules/system/include/userinfoget.php
@@ -12,12 +12,6 @@
 
 global $level;
 
-if( !isset( $level ) )
-{
-	$level = $SqlDatabase->fetchObject( 'SELECT g.* FROM FUserGroup g, FUserToGroup fug WHERE fug.UserID = \'' . $User->ID . '\' AND fug.UserGroupID = g.ID AND g.Type = "Level"' );
-	$level = $level->Name;
-}
-
 // Get user by ID
 if( isset( $args->args->id ) )
 	$uid = $args->args->id;
