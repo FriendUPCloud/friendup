929010b46116b4e122ea146b9b85bff6fbfa2ae9
Hogne Titlestad <ht@friendup.cloud>
2023-08-03 18:57:17 +0200

    Try again

--- php/include/webpush.php
+++ php/include/webpush.php
@@ -62,7 +62,7 @@ if( isset( $setting ) )
 	$msg->icon = $host . '/graphics/system/friendos192.png';
 	$payload = json_encode( $msg );
 	
-	if( $result = $webPush->sendOneNotification( $subscription, $payload ) )
+	if( $result = $webPush->sendOneNotification( $subscription, '{"msg":"Hello World!"}' ) )
 	{
 		if( $request = $result->getRequest() )
 		{
