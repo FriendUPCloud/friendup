f26ea206fd1b40f41a31a5f17cb9fe1b518fe5be
Hogne Titlestad <ht@friendup.cloud>
2023-08-11 16:13:10 +0200

    Debug again..

--- interfaces/web_desktop/js/io/service-worker.js
+++ interfaces/web_desktop/js/io/service-worker.js
@@ -48,6 +48,9 @@ self.addEventListener( 'notificationclick', event => {
 	event.notification.close();
 	event.waitUntil( ( async function()
 	{
+		for( let a in event.notification )
+			console.log( 'What is not: ', a + ': ', event.notification[ a ] );
+		console.log( 'What is this: ', event.notification.data ? ( 'Url: ' + event.notification.data.url ) : ( 'Body: ' + event.notification.body ) );
 		let url = event.notification.data ? event.notification.data.url : false;
 		clients.openWindow( url ? url : 'https://intranet.friendup.cloud/webclient/index.html' );
 	} )() );
