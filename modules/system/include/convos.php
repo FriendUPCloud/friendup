1d05d70927f970447a79ce8887c753dbc6556472
Hogne Titlestad <ht@friendup.cloud>
2023-08-22 21:59:12 +0200

    Fix

--- modules/system/include/convos.php
+++ modules/system/include/convos.php
@@ -313,7 +313,7 @@ if( isset( $args->args ) )
 	                    $out->Name = $v->Name;
                     $out->Message = $v->Message;
                     $out->Date = $v->Date;
-                    $out->Timestamp = strtotime( $v->Data );
+                    $out->Timestamp = strtotime( $v->Date );
                     $out->Own = false;
                     $out->Seen = $v->Seen;
                     if( isset( $v->UniqueID ) )
